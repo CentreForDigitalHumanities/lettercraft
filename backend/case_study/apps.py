d9c629ab08fbc6117dde9f83b2e7c8dc4d7fc277
Luka van der Plas <lukavdplas@mailfence.com>
2024-01-31 17:26:37 +0100

    add verbose app names

--- backend/case_study/apps.py
+++ backend/case_study/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class CaseStudyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'case_study'
+    verbose_name = 'case studies'
--- backend/letter/apps.py
+++ backend/letter/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class LetterConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'letter'
+    verbose_name = 'letters'
