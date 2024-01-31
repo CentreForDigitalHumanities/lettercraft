160ff0b60aa0569d90b7d2e826a678279718dea6
Luka van der Plas <lukavdplas@mailfence.com>
2024-01-31 18:12:46 +0100

    add name and category to letters

--- backend/letter/models.py
+++ backend/letter/models.py
@@ -3,8 +3,32 @@ from core.models import Field
 
 
 class Letter(models.Model):
+    name = models.CharField(
+        max_length=200,
+        blank=False,
+        unique=True,
+        help_text="a unique name to identify this letter in the database",
+    )
+
     def __str__(self):
-        return f"letter #{self.id}"
+        return self.name
+
+
+class Category(models.Model):
+    label = models.CharField(max_length=200, blank=False, null=False, unique=True)
+    description = models.TextField(blank=True, null=False)
+
+    def __str__(self):
+        return self.label
+
+
+class LetterCategory(Field, models.Model):
+    category = models.ForeignKey(to=Category, null=True, on_delete=models.SET_NULL)
+    letter = models.OneToOneField(
+        to=Letter,
+        on_delete=models.CASCADE,
+        null=False,
+    )
 
 
 class LetterMaterial(Field, models.Model):
