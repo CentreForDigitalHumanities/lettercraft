0a89095a3397e26b72ad1761d0ecdf49a189c917
Luka van der Plas <lukavdplas@mailfence.com>
2024-02-26 16:57:23 +0100

    fill in agent form

--- frontend/src/app/data-entry/agent/agent.component.ts
+++ frontend/src/app/data-entry/agent/agent.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faCancel, faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'lc-agent',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./agent.component.scss']
 })
 export class AgentComponent {
-
+    icons = {
+        confirm: faCheck,
+        cancel: faCancel,
+        remove: faTimes,
+    }
 }
