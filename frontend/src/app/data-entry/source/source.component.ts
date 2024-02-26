61cd96414949886715f449c9e8f2075beda9f697
Luka van der Plas <lukavdplas@mailfence.com>
2024-02-26 15:56:35 +0100

    add icons

--- frontend/src/app/data-entry/source/source.component.ts
+++ frontend/src/app/data-entry/source/source.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { faCaretDown, faCaretUp, faPencil, faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faCaretDown, faCaretUp, faCube, faEnvelope, faHand, faLocationDot, faPencil, faPeopleGroup, faPerson, faPlus, faTrash, } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'lc-source',
@@ -14,6 +14,12 @@ export class SourceComponent {
         delete: faTrash,
         edit: faPencil,
         create: faPlus,
+        person: faPerson,
+        group: faPeopleGroup,
+        place: faLocationDot,
+        letter: faEnvelope,
+        object: faCube,
+        action: faHand,
     }
 
 }
