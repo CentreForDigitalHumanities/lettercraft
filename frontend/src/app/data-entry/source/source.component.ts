ac937b8f6c685ded716a86fd1399ea2993510f6b
Luka van der Plas <lukavdplas@mailfence.com>
2024-02-26 15:22:29 +0100

    source template

--- frontend/src/app/data-entry/source/source.component.ts
+++ frontend/src/app/data-entry/source/source.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faCaretDown, faCaretUp, faPencil, faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'lc-source',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./source.component.scss']
 })
 export class SourceComponent {
+
+    icons = {
+        hide: faCaretUp,
+        show: faCaretDown,
+        delete: faTrash,
+        edit: faPencil,
+        create: faPlus,
+    }
 
 }
