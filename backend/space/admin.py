0272c15a6ec83f8a69da820240abaed3348e7f78
Luka van der Plas <lukavdplas@mailfence.com>
2024-02-28 18:37:59 +0100

    add admin form

--- backend/space/admin.py
+++ backend/space/admin.py
@@ -1,3 +1,64 @@
 from django.contrib import admin
+from source.admin import ReferenceInlineAdmin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.PoliticalRegion)
+class PoliticalRegion(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.EcclesiasticalRegion)
+class EccleciasticalRegion(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.GeographicalRegion)
+class GeographicalRegionAdmin(admin.ModelAdmin):
+    pass
+
+
+class PoliticalRegionFieldInlineAdmin(admin.StackedInline):
+    verbose_name = "Political region reference"
+    model = models.PoliticalRegionField
+    fields = ["space", "political_region", "certainty", "note"]
+    extra = 0
+
+
+class EcclesiasticalRegionFieldInlineAdmin(admin.StackedInline):
+    verbose_name = "Ecclesiastical region reference"
+    model = models.EcclesiasticalRegionField
+    fields = ["space", "ecclesiastical_region", "certainty", "note"]
+    extra = 0
+
+
+class GeographicalRegionFieldInlineAdmin(admin.StackedInline):
+    verbose_name = "Geographical region reference"
+    model = models.GeographicalRegionField
+    fields = ["space", "geographical_region", "certainty", "note"]
+    extra = 0
+
+
+class LandscapeFeatureInlineAdmin(admin.StackedInline):
+    model = models.LandscapeFeature
+    fields = ["landscape", "certainty", "note"]
+    extra = 0
+
+
+class SpotInlineAdmin(admin.StackedInline):
+    model = models.Spot
+    fields = ["spot", "certainty", "note"]
+    extra = 0
+
+
+@admin.register(models.SpaceDescription)
+class SpaceDescriptionAdmin(admin.ModelAdmin):
+    fields = ["name", "description"]
+    inlines = [
+        PoliticalRegionFieldInlineAdmin,
+        EcclesiasticalRegionFieldInlineAdmin,
+        GeographicalRegionFieldInlineAdmin,
+        LandscapeFeatureInlineAdmin,
+        SpotInlineAdmin,
+        ReferenceInlineAdmin,
+    ]
