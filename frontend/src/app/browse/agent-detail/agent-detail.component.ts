a7f274b5c72e29af6b1f17f215de850cf66fc300
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2024-03-08 15:33:18 +0100

    Agent detail view

--- frontend/src/app/browse/agent-detail/agent-detail.component.ts
+++ frontend/src/app/browse/agent-detail/agent-detail.component.ts
@@ -6,7 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./agent-detail.component.scss']
 })
 export class AgentDetailComponent {
-    selectedSources: number[] = [1];
+    selectedSources: number[] = [0, 1, 2];
+
+    sources = [{
+        author: 'Fortunatus',
+        work: 'Vita Sanctae Radegundis',
+        part: 'Bk. 2',
+        pages: '1299--1301',
+        name: 'St. Radegund',
+        status: [{
+            name: 'Abbess of the Convent of the Holy Cross',
+            date: 'c. 550--587'
+        }, {
+            name: 'Saint',
+            date: '587'
+        }],
+        dateOfBirth: '',
+        dateOfDeath: '587',
+        gender: 'woman',
+    }, {
+        author: 'Gaius Sidonius',
+        work: 'Radegundis Regina',
+        part: 'Ch. 5',
+        pages: '1--3',
+        name: '',
+        status: [],
+        dateOfBirth: '510',
+        dateOfDeath: 'c. 587--590',
+        gender: 'woman',
+    }, {
+        author: 'Aurelius Cassiodorus',
+        work: 'Miracula et Virtutes Sanctissimae Radegundis',
+        part: 'Bk. 3',
+        pages: '5--7, 18--19',
+        name: 'Radegundis',
+        status: [{
+            name: 'Abbess of the Convent of the Holy Cross',
+            date: 'c. 545--588'
+        }],
+        dateOfBirth: '',
+        dateOfDeath: '588',
+        gender: 'woman'
+    }]
 
     selectSource(sourceId: number): void {
         if (this.selectedSources.includes(sourceId)) {
