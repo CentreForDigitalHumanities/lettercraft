27428a87d4a9fb09a543d6295870b47eeceedf36
Luka van der Plas <lukavdplas@mailfence.com>
2024-03-15 11:26:07 +0100

    draft space form

--- frontend/src/app/data-entry/space/space.component.ts
+++ frontend/src/app/data-entry/space/space.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { faCancel, faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { Observable, OperatorFunction, debounceTime, distinctUntilChanged, map } from 'rxjs';
 
 @Component({
   selector: 'lc-space',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./space.component.scss']
 })
 export class SpaceComponent {
+    icons = {
+        confirm: faCheck,
+        cancel: faCancel,
+        remove: faTimes,
+    }
 
+    politicalRegions = [
+        'Burgundy',
+        'Provence',
+        'Austrasia',
+        'Aquitaine',
+        'Neustria',
+        'Alamannia',
+        'Bavaria',
+    ];
+
+    searchPoliticalRegion: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) =>
+        text$.pipe(
+            debounceTime(200),
+            distinctUntilChanged(),
+            map(term => this.findMatches(term, this.politicalRegions))
+        );
+
+    private findMatches(term: string, values: string[]): string[] {
+        if (term.length > 2) {
+            const match = (value: string) => value.toLowerCase().startsWith(term.toLowerCase());
+            return values.filter(match);
+        }
+        return [];
+    }
 }
