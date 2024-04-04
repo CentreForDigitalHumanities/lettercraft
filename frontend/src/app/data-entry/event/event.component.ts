7698ff6ec04be92dd8c6cb91cd4bde31eaac63cd
Luka van der Plas <lukavdplas@mailfence.com>
2024-04-04 14:05:31 +0200

    fill in event form

--- frontend/src/app/data-entry/event/event.component.ts
+++ frontend/src/app/data-entry/event/event.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faPencil, faPlus, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'lc-event',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./event.component.scss']
 })
 export class EventComponent {
+    icons = {
+        remove: faTimes,
+        edit: faPencil,
+        add: faPlus,
+    };
 
+    labels = [
+        'writing',
+        'reading',
+        'secret communication',
+        'penance',
+    ];
 }
