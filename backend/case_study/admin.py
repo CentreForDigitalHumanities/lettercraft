7f6453bdb8390bce5926b4b33b01c1883d99ebef
Luka van der Plas <lukavdplas@mailfence.com>
2024-04-08 17:48:19 +0200

    update case study model

--- backend/case_study/admin.py
+++ backend/case_study/admin.py
@@ -2,16 +2,11 @@ from django.contrib import admin
 from . import models
 
 
-class CaseStudyEpisodeAdmin(admin.StackedInline):
-    model = models.Episode.case_studies.through
-    extra = 0
-
-
 @admin.register(models.CaseStudy)
 class CaseStudyAdmin(admin.ModelAdmin):
-    inlines = [CaseStudyEpisodeAdmin]
-
+    filter_horizontal = ["key_figures", "key_sites", "episodes"]
 
 @admin.register(models.Episode)
 class EpisodeAdmin(admin.ModelAdmin):
-    filter_horizontal = ["case_studies", "event_descriptions"]
+    fields = ["name", "description", "event_descriptions"]
+    filter_horizontal = ["event_descriptions"]
--- backend/case_study/models.py
+++ backend/case_study/models.py
@@ -1,5 +1,29 @@
 from django.db import models
 from event import models as event_models
+from person import models as person_models
+from space import models as space_models
+
+
+class Episode(models.Model):
+    """
+    An episode is a collection of epistolary events
+    """
+
+    name = models.CharField(
+        max_length=256,
+    )
+    description = models.TextField(
+        blank=True,
+    )
+    event_descriptions = models.ManyToManyField(
+        to=event_models.EventDescription,
+        blank=True,
+        related_name="episodes",
+    )
+
+    def __str__(self):
+        return self.name
+
 
 class CaseStudy(models.Model):
     """
@@ -16,30 +40,27 @@ class CaseStudy(models.Model):
     )
     description = models.TextField(blank=True)
 
-    def __str__(self):
-        return self.name
-
-
-class Episode(models.Model):
-    """
-    An episode is a collection of epistolary events
-    """
-
-    name = models.CharField(
-        max_length=256,
-    )
-    description = models.TextField(
+    key_figures = models.ManyToManyField(
+        to=person_models.Person,
+        limit_choices_to={"identifiable": True},
         blank=True,
+        related_name="case_studies",
+        help_text="historical figures central to this case study",
     )
-    case_studies = models.ManyToManyField(
-        to=CaseStudy,
+
+    key_sites = models.ManyToManyField(
+        to=space_models.Structure,
+        limit_choices_to={"identifiable": True},
         blank=True,
-        related_name="episodes",
+        related_name="case_studies",
+        help_text="historical sites central to this case study",
     )
-    event_descriptions = models.ManyToManyField(
-        to=event_models.EventDescription,
+
+    episodes = models.ManyToManyField(
+        to=Episode,
         blank=True,
-        related_name="episodes",
+        related_name="case_studies",
+        help_text="episodes that are part of this case study",
     )
 
     def __str__(self):
