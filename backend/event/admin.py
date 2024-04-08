71623d5f047b856f4ad5133bf7c13ead8fffee79
Luka van der Plas <lukavdplas@mailfence.com>
2024-04-08 16:48:29 +0200

    update event admin form

--- backend/event/admin.py
+++ backend/event/admin.py
@@ -1,80 +1,56 @@
 from django.contrib import admin
-from django.urls import reverse
-
-from space.models import SpaceDescription, SpaceDescription
-from event.models import EventDescription
-from letter.models import GiftDescription, LetterDescription
 
 from . import models
-from core.admin import description_source_fieldset
-from django.utils.html import format_html
+from core.admin import (
+    description_id_fieldset,
+    description_source_fieldset,
+    description_field_fields,
+)
 
 
 class EventDateAdmin(admin.StackedInline):
     model = models.EventDescriptionDate
-    fields = ["year_exact", "year_lower", "year_upper", "certainty", "note"]
+    fields = ["year_exact", "year_lower", "year_upper"] + description_field_fields
     verbose_name = "date"
     verbose_name_plural = "dates"
+    extra = 0
 
 
 class EventDescriptionAgentAdmin(admin.StackedInline):
     model = models.EventDescriptionAgent
-    fields = [
-        "agent",
-        "description",
-        "certainty",
-        "note",
-    ]
+    fields = ["agent"] + description_field_fields
     extra = 0
-    verbose_name = "agent/role"
-    verbose_name_plural = "agents/roles involved"
+    verbose_name = "agent"
+    verbose_name_plural = "agents involved"
 
 
 class EventDescriptionLettersAdmin(admin.StackedInline):
     model = models.EventDescriptionLetter
+    fields = ["letter"] + description_field_fields
     extra = 0
     verbose_name = "letter"
     verbose_name_plural = "letters"
 
-    # def formfield_for_foreignkey(self, db_field, request, **kwargs):
-    #     if db_field.name == "letteractionbase":
-    #         kwargs["queryset"] = LetterAction.objects.all()
-    #     if db_field.name == "letterbase":
-    #         kwargs["queryset"] = Letter.objects.all()
-    #     return super().formfield_for_foreignkey(db_field, request, **kwargs)
-
 
 class EventDescriptionGiftsAdmin(admin.StackedInline):
     model = models.EventDescriptionGift
+    fields = ["gift"] + description_field_fields
     extra = 0
     verbose_name = "gift"
     verbose_name_plural = "gifts"
 
-    # def formfield_for_foreignkey(self, db_field, request, **kwargs):
-    #     if db_field.name == "letteractionbase":
-    #         kwargs["queryset"] = LetterAction.objects.all()
-    #     if db_field.name == "giftbase":
-    #         kwargs["queryset"] = Gift.objects.all()
-    #     return super().formfield_for_foreignkey(db_field, request, **kwargs)
-
 
 class EventDescriptionSpaceAdmin(admin.StackedInline):
     model = models.EventDescriptionSpace
+    fields = ["space"] + description_field_fields
     extra = 0
     verbose_name = "space description"
     verbose_name_plural = "space descriptions"
 
-    # def formfield_for_foreignkey(self, db_field, request, **kwargs):
-    #     if db_field.name == "letteractionbase":
-    #         kwargs["queryset"] = LetterAction.objects.all()
-    #     if db_field.name == "spacedescriptionbase":
-    #         kwargs["queryset"] = SpaceDescription.objects.all()
-    #     return super().formfield_for_foreignkey(db_field, request, **kwargs)
-
 
 @admin.register(models.EventDescription)
 class EventDescriptionAdmin(admin.ModelAdmin):
-    list_display = ["name", "description"]
+    list_display = ["name", "description", "source"]
     list_filter = ["source"]
     inlines = [
         EventDescriptionAgentAdmin,
@@ -84,4 +60,7 @@ class EventDescriptionAdmin(admin.ModelAdmin):
         EventDateAdmin,
     ]
     exclude = ["letters", "gifts"]
-    fieldsets = (description_source_fieldset,)
+    fieldsets = (
+        description_id_fieldset,
+        description_source_fieldset,
+    )
