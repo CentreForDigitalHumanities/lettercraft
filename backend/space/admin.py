418d9e01bcfd400551fe0f7855f7b23bece87131
Luka van der Plas <lukavdplas@mailfence.com>
2024-04-09 13:22:51 +0200

    admin forms

--- backend/space/admin.py
+++ backend/space/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+
 from . import models
+from core import admin as core_admin
 
 
 @admin.register(models.PoliticalRegion)
@@ -30,41 +32,46 @@ class StructureAdmin(admin.ModelAdmin):
 class PoliticalRegionFieldInlineAdmin(admin.StackedInline):
     verbose_name = "Political region reference"
     model = models.PoliticalRegionField
-    fields = ["space", "political_region", "certainty", "note"]
+    fields = ["space", "political_region"] + core_admin.description_field_fields
     extra = 0
 
 
 class EcclesiasticalRegionFieldInlineAdmin(admin.StackedInline):
     verbose_name = "Ecclesiastical region reference"
     model = models.EcclesiasticalRegionField
-    fields = ["space", "ecclesiastical_region", "certainty", "note"]
+    fields = ["space", "ecclesiastical_region"] + core_admin.description_field_fields
     extra = 0
 
 
 class GeographicalRegionFieldInlineAdmin(admin.StackedInline):
     verbose_name = "Geographical region reference"
     model = models.GeographicalRegionField
-    fields = ["space", "geographical_region", "certainty", "note"]
+    fields = ["space", "geographical_region"] + core_admin.description_field_fields
     extra = 0
 
 
 class StructureFieldInlineAdmin(admin.StackedInline):
     verbose_name = "Structure reference"
     model = models.StructureField
-    fields = ["space", "structure", "certainty", "note"]
+    fields = ["space", "structure"] + core_admin.description_field_fields
     extra = 0
 
 
 class LandscapeFeatureInlineAdmin(admin.StackedInline):
     model = models.LandscapeFeature
-    fields = ["landscape", "certainty", "note"]
+    fields = ["landscape"] + core_admin.description_field_fields
     extra = 0
 
 
 @admin.register(models.SpaceDescription)
 class SpaceDescriptionAdmin(admin.ModelAdmin):
-    list_display = ["name", "description"]
-    fields = ["name", "description"]
+    list_display = ["name", "description", "source"]
+    list_filter = ["source"]
+    search_fields = ["name", "description"]
+    fieldsets = [
+        core_admin.named_fieldset,
+        core_admin.description_source_fieldset,
+    ]
     inlines = [
         PoliticalRegionFieldInlineAdmin,
         EcclesiasticalRegionFieldInlineAdmin,
