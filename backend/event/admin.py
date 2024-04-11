53b9bb684378133d51b9384126cb27c458ea2766
Luka van der Plas <lukavdplas@mailfence.com>
2024-04-11 15:06:05 +0200

    event description model

--- backend/event/admin.py
+++ backend/event/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
 from . import models
+from core import admin as core_admin
 
+
+class EventDescriptionAgentAdmin(admin.StackedInline):
+    model = models.EventDescriptionAgent
+    fields = ["agent"] + core_admin.description_field_fields
+    extra = 0
+    verbose_name = "involved agent"
+
+
+class EventDescriptionGiftAdmin(admin.StackedInline):
+    model = models.EventDescriptionGift
+    fields = ["gift"] + core_admin.description_field_fields
+    extra = 0
+    verbose_name = "involved gift"
+
+
+class EventDescriptionLetterAdmin(admin.StackedInline):
+    model = models.EventDescriptionLetter
+    fields = ["letter"] + core_admin.description_field_fields
+    extra = 0
+    verbose_name = "involved letter"
+
+
+class EventDescriptionSpaceAdmin(admin.StackedInline):
+    model = models.EventDescriptionSpace
+    fields = ["space"] + core_admin.description_field_fields
+    extra = 0
+    verbose_name = "involved space"
+
+
+@admin.register(models.EventDescription)
+class EventDescriptionAdmin(core_admin.EntityDescriptionAdmin, admin.ModelAdmin):
+    inlines = [
+        EventDescriptionAgentAdmin,
+        EventDescriptionGiftAdmin,
+        EventDescriptionLetterAdmin,
+        EventDescriptionSpaceAdmin,
+    ]
--- backend/event/models.py
+++ backend/event/models.py
@@ -1,8 +1,126 @@
 from django.db import models
-from django.contrib import admin
+from django.core.exceptions import ValidationError
 
-from core.models import Field, LettercraftDate
-from case_study.models import CaseStudy
-# from person.models import Agent
-# from letter.models import Gift, Letter
+from core.models import EntityDescription, DescriptionField
+from person.models import AgentDescription
+from letter.models import GiftDescription, LetterDescription
 from space.models import SpaceDescription
+
+
+class EventDescription(EntityDescription, models.Model):
+    """
+    An epistolary event described in as source text
+    """
+
+    agents = models.ManyToManyField(
+        to=AgentDescription,
+        through="EventDescriptionAgent",
+        blank=True,
+        help_text="agents involved in this event",
+    )
+    gifts = models.ManyToManyField(
+        to=GiftDescription,
+        through="EventDescriptionGift",
+        blank=True,
+        help_text="gifts involved in this event",
+    )
+    letters = models.ManyToManyField(
+        to=LetterDescription,
+        through="EventDescriptionLetter",
+        blank=True,
+        help_text="letters involved in this event",
+    )
+    spaces = models.ManyToManyField(
+        to=SpaceDescription,
+        through="EventDescriptionSpace",
+        blank=True,
+        help_text="locations involved in this event",
+    )
+
+
+class EventDescriptionAgent(DescriptionField, models.Model):
+    """
+    Relationship between an agent and an event described in a source text
+    """
+
+    event = models.ForeignKey(
+        to=EventDescription,
+        on_delete=models.CASCADE,
+    )
+    agent = models.ForeignKey(
+        to=AgentDescription,
+        on_delete=models.CASCADE,
+    )
+
+    def clean(self):
+        if self.event.source != self.agent.source:
+            raise ValidationError("Can only link descriptions in the same source text")
+
+    def __str__(self):
+        return f"{self.agent.name} / {self.event}"
+
+
+class EventDescriptionGift(DescriptionField, models.Model):
+    """
+    Relationship between an agent and an gift described in a source text
+    """
+
+    event = models.ForeignKey(
+        to=EventDescription,
+        on_delete=models.CASCADE,
+    )
+    gift = models.ForeignKey(
+        to=GiftDescription,
+        on_delete=models.CASCADE,
+    )
+
+    def clean(self):
+        if self.event.source != self.gift.source:
+            raise ValidationError("Can only link descriptions in the same source text")
+
+    def __str__(self):
+        return f"{self.gift.name} / {self.event}"
+
+
+class EventDescriptionLetter(DescriptionField, models.Model):
+    """
+    Relationship between an agent and a letter described in a source text
+    """
+
+    event = models.ForeignKey(
+        to=EventDescription,
+        on_delete=models.CASCADE,
+    )
+    letter = models.ForeignKey(
+        to=LetterDescription,
+        on_delete=models.CASCADE,
+    )
+
+    def clean(self):
+        if self.event.source != self.letter.source:
+            raise ValidationError("Can only link descriptions in the same source text")
+
+    def __str__(self):
+        return f"{self.letter.name} / {self.event}"
+
+
+class EventDescriptionSpace(DescriptionField, models.Model):
+    """
+    Relationship between an agent and a space described in a source text
+    """
+
+    event = models.ForeignKey(
+        to=EventDescription,
+        on_delete=models.CASCADE,
+    )
+    space = models.ForeignKey(
+        to=SpaceDescription,
+        on_delete=models.CASCADE,
+    )
+
+    def clean(self):
+        if self.event.source != self.space.source:
+            raise ValidationError("Can only link descriptions in the same source text")
+
+    def __str__(self):
+        return f"{self.space.name} / {self.event}"
