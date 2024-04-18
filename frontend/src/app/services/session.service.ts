2fc7dbe2f0decbbcd8dc0d0bca3a599d2bcae0d7
Luka van der Plas <lukavdplas@mailfence.com>
2024-04-18 16:46:56 +0200

    add session service

---
+++ frontend/src/app/services/session.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class SessionService {
+    private static sessionExpiredSubject = new Subject<void>();
+    public expired = SessionService.sessionExpiredSubject.asObservable();
+
+    public static markExpired() {
+        SessionService.sessionExpiredSubject.next();
+    }
+}
