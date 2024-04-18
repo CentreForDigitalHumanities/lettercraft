2a81c7b0408400cbd6de1cf32d1675641c860cc8
Luka van der Plas <lukavdplas@mailfence.com>
2024-04-18 16:50:32 +0200

    add auth service spec

--- frontend/src/app/services/auth.service.spec.ts
+++ frontend/src/app/services/auth.service.spec.ts
@@ -1,16 +1,25 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AuthService } from './auth.service';
+import { SessionService } from './session.service';
 
 describe('AuthService', () => {
-  let service: AuthService;
+    let service: AuthService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule, RouterTestingModule],
+            providers: [
+                SessionService,
+            ],
+        });
+        service = TestBed.inject(AuthService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
 });
