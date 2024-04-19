10922a6ed536a68f322c22687d8df113935c5795
Luka van der Plas <lukavdplas@mailfence.com>
2024-04-19 12:26:44 +0200

    generate login component

--- frontend/src/app/routes.ts
+++ frontend/src/app/routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './user/login/login.component';
 
 const routes: Routes = [
     {
         path: 'home',
         component: HomeComponent,
+    },
+    {
+        path: 'login',
+        component: LoginComponent,
     },
     {
         path: '',
---
+++ frontend/src/app/user/login/login.component.spec.ts
@@ -0,0 +1,26 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LoginComponent } from './login.component';
+import { AuthService } from 'src/app/services/auth.service';
+import { HttpTestingController } from '@angular/common/http/testing';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+describe('LoginComponent', () => {
+    let component: LoginComponent;
+    let fixture: ComponentFixture<LoginComponent>;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [LoginComponent],
+            providers: [AuthService, HttpTestingController],
+            imports: [SharedModule]
+        });
+        fixture = TestBed.createComponent(LoginComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
