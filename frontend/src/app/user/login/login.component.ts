2865ed0ac62877be95647b58242a4256e1634f22
Luka van der Plas <lukavdplas@mailfence.com>
2024-04-22 12:30:03 +0200

    validate login input

--- frontend/src/app/user/login/login.component.ts
+++ frontend/src/app/user/login/login.component.ts
@@ -1,8 +1,9 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '@services/auth.service';
+import { passwordValidators, usernameValidators } from '../validation';
 
 
 
@@ -12,8 +13,16 @@ import { AuthService } from '@services/auth.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-    usernameInput = new FormControl();
-    passwordInput = new FormControl();
+    usernameInput = new FormControl('', [
+        Validators.required,
+        ...usernameValidators
+    ]);
+    usernameErrorMessage = '';
+    passwordInput = new FormControl('', [
+        Validators.required,
+        ...passwordValidators,
+    ]);
+    passwordErrorMessage = '';
 
     requestFailed = false;
 
@@ -28,19 +37,46 @@ export class LoginComponent {
     }
 
     submit() {
-        this.authService.login(
-            this.usernameInput.value, this.passwordInput.value
-        ).subscribe({
-            next: () => this.loginSucces(),
-            error: (e: HttpErrorResponse) => this.loginFailed(e)
-        });
+        const valid = this.checkValidation();
+        if (valid) {
+            this.authService.login(
+                this.usernameInput.value as string, this.passwordInput.value as string
+            ).subscribe({
+                next: () => this.loginSucces(),
+                error: (e: HttpErrorResponse) => this.loginFailed(e)
+            });
+        }
     }
 
-    loginSucces() {
+    private checkValidation() {
+        if (this.usernameInput.invalid) {
+            if (this.usernameInput.errors?.['required']) {
+                this.usernameErrorMessage = 'username is required';
+            } else {
+                this.usernameErrorMessage = 'invalid username';
+            }
+        } else {
+            this.usernameErrorMessage = '';
+        }
+
+        if (this.passwordInput.invalid) {
+            if (this.passwordInput.errors?.['required']) {
+                this.passwordErrorMessage = 'password is required';
+            } else {
+                this.passwordErrorMessage = 'invalid password';
+            }
+        } else {
+            this.passwordErrorMessage = '';
+        }
+
+        return this.usernameInput.valid && this.passwordInput.valid;
+    }
+
+    private loginSucces() {
         this.router.navigate([this.returnUrl]);
     }
 
-    loginFailed(error: HttpErrorResponse) {
+    private loginFailed(error: HttpErrorResponse) {
         this.requestFailed = true;
     }
 }
