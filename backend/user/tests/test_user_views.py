0a89ef422ab17464de5f0e8bbb8cbc2e90b29828
Luka van der Plas <lukavdplas@mailfence.com>
2024-04-23 12:48:33 +0200

    add test for readonly field

--- backend/user/tests/test_user_views.py
+++ backend/user/tests/test_user_views.py
@@ -1,7 +1,7 @@
 from unittest.mock import ANY
 
 
-def test_user_serializer(user_client, user_data):
+def test_user_details(user_client, user_data):
     details = user_client.get("/users/user/")
     assert details.status_code == 200
     assert details.data == {
@@ -18,11 +18,20 @@ def test_user_updates(user_client, user_data):
     route = "/users/user/"
     details = lambda: user_client.get(route).data
     assert details()["username"] == user_data["username"]
+
+    # update username should succeed
     response = user_client.patch(
         route,
         {"username": "NewName"},
         content_type="application/json",
     )
     assert response.status_code == 200
+    assert details()["username"] == "NewName"
 
-    assert not details()["username"] == user_data["username"]
+    # is_staff is readonly, so nothing should happen
+    response = user_client.patch(
+        route,
+        {"is_staff": True},
+        content_type="application/json",
+    )
+    assert not details()["is_staff"]
