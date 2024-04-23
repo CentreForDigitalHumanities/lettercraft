6d71292e0cb56f82119fe369b4bbef0c7b6ed670
Luka van der Plas <lukavdplas@mailfence.com>
2024-04-23 11:29:41 +0200

    add tests for user menu

--- frontend/src/app/core/menu/user-menu/user-menu.component.spec.ts
+++ frontend/src/app/core/menu/user-menu/user-menu.component.spec.ts
@@ -1,20 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { UserMenuComponent } from './user-menu.component';
 import { AuthService } from '@services/auth.service';
-import { AuthServiceMock } from 'src/mock/auth.service.mock';
+import { AuthServiceMock } from '@mock/auth.service.mock';
+import { SharedTestingModule } from '@shared/shared-testing.module';
 
 describe('UserMenuComponent', () => {
     let component: UserMenuComponent;
     let fixture: ComponentFixture<UserMenuComponent>;
+    let authService: AuthServiceMock;
+
+    const spinner = () => fixture.debugElement.query(By.css('.spinner-border'));
+    const signInLink = () => fixture.debugElement.query(By.css('a[href="/login"]'));
+    const userDropdownTrigger = () => fixture.debugElement.query(By.css('#userDropdown'))
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             providers: [
                 { provide: AuthService, useClass: AuthServiceMock }
             ],
-            declarations: [UserMenuComponent]
+            declarations: [UserMenuComponent],
+            imports: [SharedTestingModule],
         });
+        authService = TestBed.inject(AuthService) as any as AuthServiceMock;
         fixture = TestBed.createComponent(UserMenuComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
@@ -22,5 +31,29 @@ describe('UserMenuComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should show a loading spinner', () => {
+        expect(spinner()).toBeTruthy();
+        expect(signInLink()).toBeFalsy();
+        expect(userDropdownTrigger()).toBeFalsy();
+    });
+
+    it('should show sign-in when not logged in', () => {
+        authService.setUser(null);
+        fixture.detectChanges();
+
+        expect(spinner()).toBeFalsy();
+        expect(signInLink()).toBeTruthy();
+        expect(userDropdownTrigger()).toBeFalsy();
+    });
+
+    it('should show a user menu when logged in', () => {
+        authService.setUser({ username: 'test', id: 0, isStaff: true });
+        fixture.detectChanges();
+
+        expect(spinner()).toBeFalsy();
+        expect(signInLink()).toBeFalsy();
+        expect(userDropdownTrigger()).toBeTruthy();
     });
 });
--- frontend/src/mock/auth.service.mock.ts
+++ frontend/src/mock/auth.service.mock.ts
@@ -3,4 +3,8 @@ import { User } from "../app/models/user";
 
 export class AuthServiceMock {
     currentUser$ = new BehaviorSubject<User | null | undefined>(undefined);
+
+    setUser(value: User | null | undefined): void {
+        this.currentUser$.next(value);
+    }
 }
