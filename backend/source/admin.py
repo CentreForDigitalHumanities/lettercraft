7642e497642189dd46dc48bb7ee7de305d6b0246
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2024-04-25 14:53:07 +0200

    Admin updates

--- backend/source/admin.py
+++ backend/source/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from django.contrib.contenttypes.admin import GenericStackedInline
 from . import models
+
 
 @admin.register(models.Source)
 class SourceAdmin(admin.ModelAdmin):
-    fields = ["name", "bibliographical_info"]
+    fields = [
+        "name",
+        "medieval_title",
+        "medieval_author",
+        "edition_title",
+        "edition_author",
+    ]
