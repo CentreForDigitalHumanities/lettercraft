596907c549388567c8e21d97a83d7679b1a13544
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2024-04-25 15:30:27 +0200

    Add date fields to admin

--- backend/source/admin.py
+++ backend/source/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 from . import models
+from core import admin as core_admin
+
+
+class SourceWrittenDateAdmin(admin.StackedInline):
+    model = models.SourceWrittenDate
+    fields = core_admin.date_fields
+    verbose_name = "Date (range) of composition"
+    extra = 0
+
+
+class SourceContentsDateAdmin(admin.StackedInline):
+    model = models.SourceContentsDate
+    fields = core_admin.date_fields
+    verbose_name = "Date (range) of described events"
+    extra = 0
 
 
 @admin.register(models.Source)
@@ -10,4 +25,8 @@ class SourceAdmin(admin.ModelAdmin):
         "medieval_author",
         "edition_title",
         "edition_author",
+    ]
+    inlines = [
+        SourceWrittenDateAdmin,
+        SourceContentsDateAdmin,
     ]
