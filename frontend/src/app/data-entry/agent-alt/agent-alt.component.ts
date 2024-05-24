f57074be27326f6f387e8f969c1bdd3ced836616
Luka van der Plas <lukavdplas@mailfence.com>
2024-05-24 12:40:14 +0200

    reverse agent episode relationship

--- frontend/src/app/data-entry/agent-alt/agent-alt.component.ts
+++ frontend/src/app/data-entry/agent-alt/agent-alt.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { agents } from '../source-alt/source-data';
+import { agents, episodes } from '../source-alt/source-data';
+import { faCaretDown, faCaretUp, faPencil, faPlus, faTimes } from '@fortawesome/free-solid-svg-icons';
+import * as _ from 'underscore';
 
 @Component({
   selector: 'lc-agent-alt',
@@ -9,9 +11,23 @@ import { agents } from '../source-alt/source-data';
 })
 export class AgentAltComponent {
     agent: any;
+    episodes: any[];
+    otherEpisodes: any[];
+
+    icons = {
+        remove: faTimes,
+        collapse: faCaretUp,
+        expand: faCaretDown,
+        edit: faPencil,
+        add: faPlus,
+    };
 
     constructor(private activatedRoute: ActivatedRoute) {
         const id = activatedRoute.snapshot.params['agentID'];
         this.agent = agents.find(a => a.id === parseInt(id));
+        this.episodes = episodes.filter(episode =>
+            _.any(episode.agents, agent => _.isEqual(agent, this.agent))
+        );
+        this.otherEpisodes = _.difference(episodes, this.episodes);
     }
 }
