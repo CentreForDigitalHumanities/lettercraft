c02b1be8de3d029d7683d60c1e7d894f9708a82b
Luka van der Plas <lukavdplas@mailfence.com>
2024-05-24 11:44:11 +0200

    draft episode form

--- frontend/src/app/data-entry/episode-alt/episode-alt.component.ts
+++ frontend/src/app/data-entry/episode-alt/episode-alt.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { episodes } from '../source-alt/source-data';
+import { faPencil, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'lc-episode-alt',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./episode-alt.component.scss']
 })
 export class EpisodeAltComponent {
+    icons = {
+        remove: faTimes,
+        edit: faPencil,
+    };
 
+    labels = [
+        'writing',
+        'reading',
+        'secret communication',
+        'penance',
+    ]
+
+    episode: any;
+
+    constructor(private activatedRoute: ActivatedRoute) {
+        const id = activatedRoute.snapshot.params['episodeID'];
+        this.episode = episodes.find(e => e.order == id);
+    }
 }
