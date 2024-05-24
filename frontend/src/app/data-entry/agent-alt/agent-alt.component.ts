46b40b0c29c10b0f7073182bba21ef50dad37817
Luka van der Plas <lukavdplas@mailfence.com>
2024-05-24 12:27:30 +0200

    agent component basics

--- frontend/src/app/data-entry/agent-alt/agent-alt.component.ts
+++ frontend/src/app/data-entry/agent-alt/agent-alt.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { agents } from '../source-alt/source-data';
 
 @Component({
   selector: 'lc-agent-alt',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./agent-alt.component.scss']
 })
 export class AgentAltComponent {
+    agent: any;
 
+    constructor(private activatedRoute: ActivatedRoute) {
+        const id = activatedRoute.snapshot.params['agentID'];
+        this.agent = agents.find(a => a.id === parseInt(id));
+    }
 }
