ef125163594cf44f8cde552a1dd096ee7f9efa4e
Luka van der Plas <lukavdplas@mailfence.com>
2024-06-12 17:03:01 +0200

    load data in location component

--- frontend/src/app/data-entry/location/location.component.ts
+++ frontend/src/app/data-entry/location/location.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { faBookmark, faCaretDown, faCaretUp, faLocationDot, faPencil, faPlus, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { episodes, locations } from '../source-alt/source-data';
+import _ from 'underscore';
 
 @Component({
   selector: 'lc-location',
@@ -6,5 +10,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./location.component.scss']
 })
 export class LocationComponent {
+    episodes: any[];
+    otherEpisodes: any[];
+    location: any;
 
+    icons = {
+        remove: faTimes,
+        collapse: faCaretUp,
+        expand: faCaretDown,
+        edit: faPencil,
+        add: faPlus,
+        episode: faBookmark,
+        location: faLocationDot,
+    };
+
+    constructor(private activatedRoute: ActivatedRoute) {
+        const id = activatedRoute.snapshot.params['locationID'];
+        this.location = locations.find(a => a.id === parseInt(id));
+        this.episodes = episodes.filter(episode =>
+            _.any(episode.locations, location => _.isEqual(location, this.location))
+        );
+        this.otherEpisodes = _.difference(episodes, this.episodes);
+    }
 }
