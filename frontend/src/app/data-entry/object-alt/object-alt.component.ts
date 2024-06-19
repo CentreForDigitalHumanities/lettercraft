99732a4b3ba51e0d8a7d25ab1c525762967e7889
Luka van der Plas <lukavdplas@mailfence.com>
2024-06-19 16:04:45 +0200

    get object data from route

--- frontend/src/app/data-entry/object-alt/object-alt.component.ts
+++ frontend/src/app/data-entry/object-alt/object-alt.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { objects } from '../source-alt/source-data';
 
 @Component({
   selector: 'lc-object-alt',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./object-alt.component.scss']
 })
 export class ObjectAltComponent {
+    object: any;
 
+    constructor(private activatedRoute: ActivatedRoute) {
+        const id = activatedRoute.snapshot.params['objectID'];
+        this.object = objects.find(o => o.id == id);
+    }
 }
