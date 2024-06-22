97f4aadb8010fe7a8e4d22217eaf5b5f92133b83
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2024-06-22 17:11:14 +0200

    Update LettercraftFieldType

--- backend/core/types/FieldType.py
+++ backend/core/types/FieldType.py
@@ -1,28 +1,16 @@
-from graphene import ObjectType, Int, ResolveInfo, String
-
 from core.models import Field
+from core.types.AbstractDjangoObjectType import AbstractDjangoObjectType
 
 
-class LettercraftFieldType(ObjectType):
+class LettercraftFieldType(AbstractDjangoObjectType):
     """
     Type for models that extend the Field model.
     Should not be queried directly, but should be extended by other types.
     """
 
-    certainty = Int(required=True)
-    note = String()
-
-    @staticmethod
-    def fields() -> list[str]:
-        return [
+    class Meta:
+        model = Field
+        fields = [
             "certainty",
             "note",
         ]
-
-    @staticmethod
-    def resolve_certainty(parent: Field, info: ResolveInfo) -> int:
-        return parent.certainty
-
-    @staticmethod
-    def resolve_note(parent: Field, info: ResolveInfo) -> str:
-        return parent.note
