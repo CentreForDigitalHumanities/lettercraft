087aaad5bfd5760e76d1425f6876e060b5575283
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2024-06-22 12:26:34 +0200

    AbstractDjangoObjectType

--- backend/core/types/HistoricalEntityType.py
+++ backend/core/types/HistoricalEntityType.py
@@ -1,20 +1,18 @@
 from graphene import Boolean, ObjectType
 
+from core.models import HistoricalEntity
+from core.types.AbstractDjangoObjectType import AbstractDjangoObjectType
 from core.types.NamedType import NamedType
 
 
-class HistoricalEntityType(NamedType, ObjectType):
+class HistoricalEntityType(NamedType, AbstractDjangoObjectType):
     """
     Type for models that extend the HistoricalEntity model.
     Should not be queried directly, but should be extended by other types.
     """
 
-    identifiable = Boolean(required=True)
-
-    @staticmethod
-    def fields() -> list[str]:
-        return [
-            "id",
-            "created_at",
-            "updated_at",
+    class Meta:
+        model = HistoricalEntity
+        fields = [
+            "identifiable",
         ] + NamedType.fields()
