d8962b409214c4984b6c1c942659c1f35413d8fd
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2024-06-22 16:46:58 +0200

    Add display_date property field resolver; remove unused GQLSourceMention enum

--- backend/core/types/DateFieldType.py
+++ backend/core/types/DateFieldType.py
@@ -1,4 +1,5 @@
 
+from graphene import ResolveInfo, String
 from core.models import LettercraftDate
 from core.types.AbstractDjangoObjectType import AbstractDjangoObjectType
 
@@ -8,6 +9,7 @@ class LettercraftDateType(AbstractDjangoObjectType):
     Type for models that extend the LettercraftDate model.
     Should not be queried directly, but should be extended by other types.
     """
+    display_date = String(required=True)
 
     class Meta:
         model = LettercraftDate
@@ -17,3 +19,7 @@ class LettercraftDateType(AbstractDjangoObjectType):
             "year_exact",
             "display_date",
         ]
+
+    @staticmethod
+    def resolve_display_date(parent: LettercraftDate, info: ResolveInfo) -> str:
+        return parent.display_date
