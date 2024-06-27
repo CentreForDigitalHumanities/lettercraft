b5884e960b4f37d696a2f387c64f93add234b55c
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2024-06-27 12:19:22 +0200

    Added types for Event app

---
+++ backend/event/queries.py
@@ -0,0 +1,26 @@
+from graphene import ID, Field, List, NonNull, ObjectType, ResolveInfo
+from django.db.models import QuerySet, Q
+from event.models import Episode
+from event.types.EpisodeType import EpisodeType
+
+
+class EventQueries(ObjectType):
+    episode = Field(EpisodeType, id=ID(required=True))
+    episodes = List(NonNull(EpisodeType), required=True, source_id=ID())
+
+    @staticmethod
+    def resolve_episode(parent: None, info: ResolveInfo, id: str) -> Episode | None:
+        try:
+            return EpisodeType.get_queryset(Episode.objects, info).get(id=id)
+        except Episode.DoesNotExist:
+            return None
+
+    @staticmethod
+    def resolve_episodes(
+        parent: None,
+        info: ResolveInfo,
+        source_id: str | None = None,
+    ) -> QuerySet[Episode]:
+        filters = Q() if source_id is None else Q(source_id=source_id)
+
+        return EpisodeType.get_queryset(Episode.objects, info).filter(filters)
--- backend/graphql_app/schema.py
+++ backend/graphql_app/schema.py
@@ -1,11 +1,12 @@
 from graphene import ObjectType, Schema
 
+from event.queries import EventQueries
 from person.queries import PersonQueries
 from source.queries import SourceQueries
 from source.mutations.UpdateOrCreateSourceMutation import UpdateOrCreateSourceMutation
 
 
-class Query(SourceQueries, PersonQueries, ObjectType):
+class Query(SourceQueries, PersonQueries, EventQueries, ObjectType):
     pass
 
 
