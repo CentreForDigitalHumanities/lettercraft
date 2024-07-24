05091aae0b0bc7046e41cebe52e6103ea55b1dd8
Luka van der Plas <lukavdplas@mailfence.com>
2024-07-24 16:45:41 +0200

    add locations/objects in episode form

--- frontend/src/app/data-entry/episode-alt/episode-alt.component.ts
+++ frontend/src/app/data-entry/episode-alt/episode-alt.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { agents, episodes } from '../source-alt/source-data';
+import { agents, episodes, locations, objects } from '../source-alt/source-data';
 import * as _ from 'underscore';
-import { icons } from '@shared/icons';
+import { agentIcon, icons } from '@shared/icons';
 
 @Component({
   selector: 'lc-episode-alt',
@@ -11,6 +11,7 @@ import { icons } from '@shared/icons';
 })
 export class EpisodeAltComponent {
     icons = icons;
+    agentIcon = agentIcon;
 
     labels = [
         'writing',
@@ -22,10 +23,14 @@ export class EpisodeAltComponent {
     episode: any;
 
     otherAgents: any[];
+    otherLocations: any[];
+    otherObjects: any[];
 
     constructor(private activatedRoute: ActivatedRoute) {
         const id = activatedRoute.snapshot.params['episodeID'];
         this.episode = episodes.find(e => e.order == id);
         this.otherAgents = _.difference(agents, this.episode.agents);
+        this.otherLocations = _.difference(locations, this.episode.locations);
+        this.otherObjects = _.difference(objects, this.episode.objects);
     }
 }
