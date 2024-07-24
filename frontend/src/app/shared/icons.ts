1bbccd5ee2fed9e38de01376f6abd57618910b44
Luka van der Plas <lukavdplas@mailfence.com>
2024-07-24 17:13:58 +0200

    declare icon names

---
+++ frontend/src/app/shared/icons.ts
@@ -0,0 +1,24 @@
+export const actionIcons = {
+    add: 'plus-lg',
+    remove: 'x-lg',
+    confirm: 'check-lg',
+    cancel: 'x-lg',
+};
+
+export const authIcons = {
+    user: 'person',
+};
+
+export const dataIcons = {
+    source: 'book',
+    episode: 'bookmark',
+    person: 'person-fill',
+    personUnknown: 'person',
+    group: 'people',
+    location: 'geo-alt',
+    gift: 'gift',
+    letter: 'envelope',
+};
+
+
+
