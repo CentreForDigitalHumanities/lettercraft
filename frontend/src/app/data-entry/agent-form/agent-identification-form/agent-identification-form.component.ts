2c4748bc67a3b4f98a251a4b9d98ae03baee7647
Luka van der Plas <lukavdplas@mailfence.com>
2024-07-31 16:13:30 +0200

    fetch data in identification form

--- frontend/src/app/data-entry/agent-form/agent-identification-form/agent-identification-form.component.ts
+++ frontend/src/app/data-entry/agent-form/agent-identification-form/agent-identification-form.component.ts
@@ -1,15 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { DataEntryAgentGQL, DataEntryAgentQuery } from 'generated/graphql';
+import { map, Subject, switchMap } from 'rxjs';
 
 @Component({
     selector: 'lc-agent-identification-form',
     templateUrl: './agent-identification-form.component.html',
     styleUrls: ['./agent-identification-form.component.scss']
 })
-export class AgentIdentificationFormComponent {
+export class AgentIdentificationFormComponent implements OnChanges, OnDestroy {
+    @Input() id?: string;
+
     form = new FormGroup({
         name: new FormControl<string>(''),
         description: new FormControl<string>(''),
         isGroup: new FormControl<boolean>(false),
     });
+
+    private id$ = new Subject<string>();
+
+    constructor(private agentQuery: DataEntryAgentGQL) {
+        this.id$.pipe(
+            switchMap(id => this.agentQuery.watch({ id }).valueChanges),
+            map(result => result.data),
+        ).subscribe(this.updateFormData.bind(this));
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes['id'] && this.id) {
+            this.id$.next(this.id);
+        }
+    }
+
+    ngOnDestroy(): void {
+        this.id$.complete();
+    }
+
+    updateFormData(data: DataEntryAgentQuery) {
+        this.form.setValue({
+            name: data.agentDescription?.name || '',
+            description: data.agentDescription?.description || '',
+            isGroup: data.agentDescription?.isGroup || false,
+        });
+    }
 }
