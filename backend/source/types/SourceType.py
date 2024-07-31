faef18c783230dc4a902c93aff762e31d236acca
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2024-07-31 10:07:35 +0200

    Add source detail query and episodes subquery

--- backend/source/types/SourceType.py
+++ backend/source/types/SourceType.py
@@ -1,13 +1,15 @@
-from graphene import Field, Int, ResolveInfo
+from graphene import Field, Int, List, ResolveInfo
 from django.db.models import QuerySet
 from graphene_django import DjangoObjectType
 from event.models import Episode
 from source.models import Source
+from event.types.EpisodeType import EpisodeType
 from source.types.SourceContentsDateType import SourceContentsDateType
 from source.types.SourceWrittenDateType import SourceWrittenDateType
 
 
 class SourceType(DjangoObjectType):
+    episodes = List(EpisodeType, required=True)
     num_of_episodes = Int(required=True)
     written_date = Field(SourceWrittenDateType)
     contents_date = Field(SourceContentsDateType)
@@ -28,6 +30,10 @@ class SourceType(DjangoObjectType):
         cls, queryset: QuerySet[Source], info: ResolveInfo
     ) -> QuerySet[Source]:
         return queryset.all()
+
+    @staticmethod
+    def resolve_episodes(parent: Source, info: ResolveInfo) -> QuerySet[Episode]:
+        return Episode.objects.filter(source_id=parent.pk)
 
     @staticmethod
     def resolve_num_of_episodes(parent: Source, info: ResolveInfo) -> int:
