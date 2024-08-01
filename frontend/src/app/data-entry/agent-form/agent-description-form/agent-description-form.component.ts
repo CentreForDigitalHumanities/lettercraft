265b62aa2f170f33292162ac317ac8d1b8203c29
Luka van der Plas <lukavdplas@mailfence.com>
2024-08-01 12:30:43 +0200

    scaffold agent description form

--- frontend/src/app/data-entry/agent-form/agent-description-form/agent-description-form.component.ts
+++ frontend/src/app/data-entry/agent-form/agent-description-form/agent-description-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, Input } from '@angular/core';
+import {
+    PersonAgentDescriptionGenderGenderChoices as GenderChoices
+} from 'generated/graphql';
+
 
 @Component({
     selector: 'lc-agent-description-form',
@@ -7,5 +11,13 @@ import { Component, Input } from '@angular/core';
 })
 export class AgentDescriptionFormComponent {
     @Input() id?: string;
+
+    genderOptions: { value: GenderChoices, label: string }[] = [
+        { value: GenderChoices.Female, label: 'Female' },
+        { value: GenderChoices.Male, label: 'Male' },
+        { value: GenderChoices.Other, label: 'Other' },
+        { value: GenderChoices.Mixed, label: 'Mixed (for groups)' },
+        { value: GenderChoices.Unknown, label: 'Unknown' }
+    ];
 
 }
