119368772e082671ffd181d780d4e185a323b380
Luka van der Plas <lukavdplas@mailfence.com>
2024-08-05 16:43:26 +0200

    fetch data in gift form component

--- frontend/src/app/data-entry/gift-form/gift-form.component.ts
+++ frontend/src/app/data-entry/gift-form/gift-form.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataIcons } from '@shared/icons';
+import { DataEntryGiftGQL, DataEntryGiftQuery } from 'generated/graphql';
+import { map, Observable, switchMap } from 'rxjs';
 
 @Component({
     selector: 'lc-gift-form',
@@ -6,5 +10,18 @@ import { Component } from '@angular/core';
     styleUrls: ['./gift-form.component.scss']
 })
 export class GiftFormComponent {
+    id$: Observable<string>;
+    data$: Observable<DataEntryGiftQuery>;
 
+    dataIcons = dataIcons;
+
+    constructor(private route: ActivatedRoute, private giftQuery: DataEntryGiftGQL) {
+        this.id$ = this.route.params.pipe(
+            map(params => params['id']),
+        );
+        this.data$ = this.id$.pipe(
+            switchMap(id => this.giftQuery.watch({ id }).valueChanges),
+            map(result => result.data),
+        );
+    }
 }
