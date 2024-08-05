0aab1314add0dd6e8b36f17e9f5cf49c42323e1a
Luka van der Plas <lukavdplas@mailfence.com>
2024-08-05 17:13:50 +0200

    fetch data in location form

--- frontend/src/app/data-entry/location-form/location-form.component.ts
+++ frontend/src/app/data-entry/location-form/location-form.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataIcons } from '@shared/icons';
+import { DataEntrySpaceDescriptionGQL, DataEntrySpaceDescriptionQuery } from 'generated/graphql';
+import { map, Observable, switchMap } from 'rxjs';
 
 @Component({
     selector: 'lc-location-form',
@@ -6,5 +10,21 @@ import { Component } from '@angular/core';
     styleUrls: ['./location-form.component.scss']
 })
 export class LocationFormComponent {
+    id$: Observable<string>;
+    data$: Observable<DataEntrySpaceDescriptionQuery>;
+
+    dataIcons = dataIcons;
+
+    constructor(
+        private route: ActivatedRoute, private spaceQuery: DataEntrySpaceDescriptionGQL
+    ) {
+        this.id$ = this.route.params.pipe(
+            map(params => params['id']),
+        );
+        this.data$ = this.id$.pipe(
+            switchMap(id => this.spaceQuery.watch({ id }).valueChanges),
+            map(result => result.data),
+        );
+    }
 
 }
