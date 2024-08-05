8be37531f3fc87074c844b1f691dc530784428c2
Luka van der Plas <lukavdplas@mailfence.com>
2024-08-05 17:07:04 +0200

    add breadcrumbs to letter form

--- frontend/src/app/data-entry/letter-form/letter-form.component.ts
+++ frontend/src/app/data-entry/letter-form/letter-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Breadcrumb } from '@shared/breadcrumb/breadcrumb.component';
 import { dataIcons } from '@shared/icons';
 import { DataEntryLetterGQL, DataEntryLetterQuery } from 'generated/graphql';
 import { map, Observable, switchMap } from 'rxjs';
@@ -23,6 +24,29 @@ export class LetterFormComponent {
             switchMap(id => this.letterQuery.watch({ id }).valueChanges),
             map(result => result.data),
         );
+    }
+
+    getBreadcrumbs(data: DataEntryLetterQuery): Breadcrumb[] {
+        if (data.letterDescription) {
+            return [
+                { link: '/', label: 'Lettercraft' },
+                { link: '/data-entry', label: 'Data entry' },
+                {
+                    link: `/data-entry/sources/${data.letterDescription.source.id}`,
+                    label: data.letterDescription.source.name
+                },
+                {
+                    link: `/data-entry/letters/${data.letterDescription.id}`,
+                    label: data.letterDescription.name
+                },
+            ];
+        } else {
+            return [
+                { link: '/', label: 'Lettercraft' },
+                { link: '/data-entry', label: 'Data entry' },
+                { link: '', label: 'Letter not found' }
+            ]
+        }
     }
 
 }
