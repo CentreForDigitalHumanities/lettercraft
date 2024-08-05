eb6b63b43a1a86c9657d100343c5ad62b4ce58f3
Luka van der Plas <lukavdplas@mailfence.com>
2024-08-05 17:00:07 +0200

    fetch data in letter form component

--- frontend/src/app/data-entry/letter-form/letter-form.component.ts
+++ frontend/src/app/data-entry/letter-form/letter-form.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataIcons } from '@shared/icons';
+import { DataEntryLetterGQL, DataEntryLetterQuery } from 'generated/graphql';
+import { map, Observable, switchMap } from 'rxjs';
 
 @Component({
     selector: 'lc-letter-form',
@@ -6,5 +10,19 @@ import { Component } from '@angular/core';
     styleUrls: ['./letter-form.component.scss']
 })
 export class LetterFormComponent {
+    id$: Observable<string>;
+    data$: Observable<DataEntryLetterQuery>;
+
+    dataIcons = dataIcons;
+
+    constructor(private route: ActivatedRoute, private letterQuery: DataEntryLetterGQL) {
+        this.id$ = this.route.params.pipe(
+            map(params => params['id']),
+        );
+        this.data$ = this.id$.pipe(
+            switchMap(id => this.letterQuery.watch({ id }).valueChanges),
+            map(result => result.data),
+        );
+    }
 
 }
