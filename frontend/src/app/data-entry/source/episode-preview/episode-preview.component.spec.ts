9e2f3f401c85e772e98c732c8fd24051864fc9e7
Luka van der Plas <lukavdplas@mailfence.com>
2024-08-06 16:49:48 +0200

    fix episode preview component test

--- frontend/src/app/data-entry/source/episode-preview/episode-preview.component.spec.ts
+++ frontend/src/app/data-entry/source/episode-preview/episode-preview.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { EpisodePreviewComponent } from "./episode-preview.component";
+import { SharedTestingModule } from "@shared/shared-testing.module";
 
 describe("EpisodePreviewComponent", () => {
     let component: EpisodePreviewComponent;
@@ -9,9 +10,23 @@ describe("EpisodePreviewComponent", () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
             declarations: [EpisodePreviewComponent],
+            imports: [SharedTestingModule],
         });
         fixture = TestBed.createComponent(EpisodePreviewComponent);
         component = fixture.componentInstance;
+        component.episode = {
+            id: '1',
+            name: 'Test',
+            description: 'Test episode',
+            summary: '',
+            book: '',
+            chapter: '',
+            page: '',
+            agents: [],
+            gifts: [],
+            letters: [],
+            spaces: [],
+        }
         fixture.detectChanges();
     });
 
