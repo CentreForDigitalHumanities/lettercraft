ca5183e7013e9dcd86dbae588e0d8e37c06fd745
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2024-08-06 17:37:49 +0200

    More temporary test harnesses

--- frontend/src/app/data-entry/label-select/label-select.component.ts
+++ frontend/src/app/data-entry/label-select/label-select.component.ts
@@ -1,33 +1,17 @@
-import { Component, computed, forwardRef, Input, signal } from "@angular/core";
+import { Component, computed, forwardRef } from "@angular/core";
 import {
     ControlValueAccessor,
     FormControl,
     NG_VALUE_ACCESSOR,
 } from "@angular/forms";
+import { actionIcons } from "@shared/icons";
 
 interface LabelOption {
     id: string;
     name: string;
 }
 
-const LABEL_OPTIONS: LabelOption[] = [
-    {
-        id: "1",
-        name: "Writing",
-    },
-    {
-        id: "2",
-        name: "Receiving",
-    },
-    {
-        id: "3",
-        name: "Sending",
-    },
-    {
-        id: "4",
-        name: "Licking",
-    },
-];
+
 
 @Component({
     selector: "lc-label-select",
@@ -42,13 +26,15 @@ const LABEL_OPTIONS: LabelOption[] = [
     ],
 })
 export class LabelSelectComponent implements ControlValueAccessor {
-    public control = new FormControl<string[]>([], { nonNullable: true });
+    public formControl = new FormControl<string[]>([], { nonNullable: true });
     public disabled = false;
 
-    public options: LabelOption[] = LABEL_OPTIONS;
+    public options: LabelOption[] = [];
+    public actionIcons = actionIcons;
+
     public selectedLabels = computed<LabelOption[]>(() => {
         const selected: LabelOption[] = [];
-        this.control.value.forEach(id => {
+        this.formControl.value.forEach(id => {
             const option = this.options.find(option => option.id === id);
             if (option) {
                 selected.push(option);
@@ -57,27 +43,40 @@ export class LabelSelectComponent implements ControlValueAccessor {
         return selected;
     });
 
-    onChange: () => void = () => { return; };
-    onTouch: () => void = () => { return; };
+    public removeLabel(labelId: string): void {
+        this.formControl.setValue(
+            this.formControl.value.filter(id => id !== labelId)
+        );
+    }
+
+    constructor() {
+        this.formControl.valueChanges.subscribe(value => {
+            console.log('Selected:', value);
+            this.onChange(value);
+            this.onTouch(value);
+        });
+    }
+
+
+    onChange: (_: unknown) => void = () => { return; };
+    onTouch: (_: unknown) => void = () => { return; };
 
     writeValue(labelIds: string[]): void {
         // Implement the logic to set the value of the control
         // based on the provided object
-        console.log('Writing value!'), labelIds;
-        this.control.setValue(labelIds);
+        this.formControl.setValue(labelIds);
     }
 
     registerOnChange(fn: () => void): void {
         // Implement the logic to register the provided function
         // as the callback to be called when the value of the control changes
-        console.log('Registering onChange!');
+        console.log('ON change!');
         this.onChange = fn;
     }
 
     registerOnTouched(fn: () => void): void {
         // Implement the logic to register the provided function
         // as the callback to be called when the control is touched
-        console.log('Registering onTouched!');
         this.onTouch = fn;
     }
 
--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,5 +1,25 @@
 import { Component, OnInit } from "@angular/core";
 import { FormControl } from "@angular/forms";
+import { MultiselectItem } from "../data-entry/shared/multiselect/multiselect.component";
+
+const ITEMS: MultiselectItem[] = [
+    {
+        id: "1",
+        label: "Writing",
+    },
+    {
+        id: "2",
+        label: "Receiving",
+    },
+    {
+        id: "3",
+        label: "Sending",
+    },
+    {
+        id: "4",
+        label: "Licking",
+    },
+];
 
 @Component({
     selector: "lc-home",
@@ -7,7 +27,9 @@ import { FormControl } from "@angular/forms";
     styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
-    public control = new FormControl<string[]>([]);
+    public control = new FormControl<string[]>([], { nonNullable: true });
+
+    items = ITEMS;
 
     ngOnInit(): void {
         this.control.setValue(["1", "3"]);
@@ -15,5 +37,9 @@ export class HomeComponent implements OnInit {
         this.control.valueChanges.subscribe((value) =>
             console.log("New value:", value)
         );
+    }
+
+    test(): void {
+        this.control.setValue(["2"])
     }
 }
