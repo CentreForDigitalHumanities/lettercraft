3303438e1a5368f8bec5ca70323d480908657e03
Luka van der Plas <lukavdplas@mailfence.com>
2024-08-06 11:44:03 +0200

    add breadcrumbs to agent component

--- frontend/src/app/data-entry/agent-form/agent-form.component.ts
+++ frontend/src/app/data-entry/agent-form/agent-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Breadcrumb } from '@shared/breadcrumb/breadcrumb.component';
 import { dataIcons } from '@shared/icons';
 import { DataEntryAgentGQL, DataEntryAgentQuery } from 'generated/graphql';
 import { map, Observable, switchMap } from 'rxjs';
@@ -23,5 +24,28 @@ export class AgentFormComponent {
             switchMap(id => this.agentQuery.watch({ id }).valueChanges),
             map(result => result.data),
         );
+    }
+
+    getBreadcrumbs(data: DataEntryAgentQuery): Breadcrumb[] {
+        if (data.agentDescription) {
+            return [
+                { link: '/', label: 'Lettercraft' },
+                { link: '/data-entry', label: 'Data entry' },
+                {
+                    link: `/data-entry/sources/${data.agentDescription.source.id}`,
+                    label: data.agentDescription.source.name
+                },
+                {
+                    link: `/data-entry/agents/${data.agentDescription.id}`,
+                    label: data.agentDescription.name
+                },
+            ];
+        } else {
+            return [
+                { link: '/', label: 'Lettercraft' },
+                { link: '/data-entry', label: 'Data entry' },
+                { link: '', label: 'Agent not found' }
+            ]
+        }
     }
 }
