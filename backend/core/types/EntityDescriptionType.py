6fde15f15b7ece3b075b1b2c62987fe39b2a600d
Luka van der Plas <lukavdplas@mailfence.com>
2024-08-06 13:29:36 +0200

    add contributors to entity description queries

--- backend/core/types/EntityDescriptionType.py
+++ backend/core/types/EntityDescriptionType.py
@@ -1,13 +1,19 @@
 from core.models import EntityDescription
 from core.types.AbstractDjangoObjectType import AbstractDjangoObjectType
 from core.types.NamedType import NamedType
+from graphene import List, ResolveInfo
+from django.db.models import QuerySet
 
+from user.models import User
+from user.types.UserType import UserType
 
 class EntityDescriptionType(NamedType, AbstractDjangoObjectType):
     """
     Type for models that extend the EntityDescription model.
     Should not be queried directly, but should be extended by other types.
     """
+
+    contributors = List(UserType)
 
     class Meta:
         model = EntityDescription
@@ -18,4 +24,11 @@ class EntityDescriptionType(NamedType, AbstractDjangoObjectType):
             "book",
             "chapter",
             "page",
+            "contributors",
         ] + NamedType.fields()
+
+    @staticmethod
+    def resolve_contributors(
+        parent: EntityDescription, info: ResolveInfo
+    ) -> QuerySet[User]:
+        return parent.contributors.all()
