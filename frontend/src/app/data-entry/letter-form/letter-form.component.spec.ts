5edae1a867104290e6b25086bb1734199c4b733b
Luka van der Plas <lukavdplas@mailfence.com>
2024-08-06 17:08:51 +0200

    fix test modules

--- frontend/src/app/data-entry/letter-form/letter-form.component.spec.ts
+++ frontend/src/app/data-entry/letter-form/letter-form.component.spec.ts
@@ -1,21 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LetterFormComponent } from './letter-form.component';
+import { SharedTestingModule } from '@shared/shared-testing.module';
 
 describe('LetterFormComponent', () => {
-  let component: LetterFormComponent;
-  let fixture: ComponentFixture<LetterFormComponent>;
+    let component: LetterFormComponent;
+    let fixture: ComponentFixture<LetterFormComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [LetterFormComponent]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [LetterFormComponent],
+            imports: [SharedTestingModule],
+        });
+        fixture = TestBed.createComponent(LetterFormComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(LetterFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- frontend/src/app/data-entry/location-form/location-form.component.spec.ts
+++ frontend/src/app/data-entry/location-form/location-form.component.spec.ts
@@ -1,21 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LocationFormComponent } from './location-form.component';
+import { SharedTestingModule } from '@shared/shared-testing.module';
 
 describe('LocationFormComponent', () => {
-  let component: LocationFormComponent;
-  let fixture: ComponentFixture<LocationFormComponent>;
+    let component: LocationFormComponent;
+    let fixture: ComponentFixture<LocationFormComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [LocationFormComponent]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [LocationFormComponent],
+            imports: [SharedTestingModule],
+        });
+        fixture = TestBed.createComponent(LocationFormComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(LocationFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
