60f25af38c48edc06f4cc37d01809de570c16783
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2024-08-06 14:13:38 +0200

    Added to module declarations

--- frontend/src/app/data-entry/data-entry.module.ts
+++ frontend/src/app/data-entry/data-entry.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { SharedModule } from "@shared/shared.module";
 import { SourcesComponent } from "./sources/sources.component";
 import { SourceComponent } from './source/source.component';
+import { EpisodePreviewComponent } from './source/episode-preview/episode-preview.component';
 
 @NgModule({
-    declarations: [SourcesComponent, SourceComponent],
+    declarations: [SourcesComponent, SourceComponent, EpisodePreviewComponent],
     imports: [SharedModule],
     exports: [SourcesComponent],
 })
