c6b9331b90cf07197ab5de2018511f0b1d6b9178
Luka van der Plas <lukavdplas@mailfence.com>
2024-08-06 11:06:55 +0200

    fetch agent data in agent form

--- frontend/src/app/data-entry/agent-form/agent-form.component.ts
+++ frontend/src/app/data-entry/agent-form/agent-form.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataIcons } from '@shared/icons';
+import { DataEntryAgentGQL, DataEntryAgentQuery } from 'generated/graphql';
+import { map, Observable, switchMap } from 'rxjs';
 
 @Component({
   selector: 'lc-agent-form',
@@ -6,5 +10,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./agent-form.component.scss']
 })
 export class AgentFormComponent {
+    id$: Observable<string>;
+    data$: Observable<DataEntryAgentQuery>;
 
+    dataIcons = dataIcons;
+
+    constructor(private route: ActivatedRoute, private agentQuery: DataEntryAgentGQL) {
+        this.id$ = this.route.params.pipe(
+            map(params => params['id']),
+        );
+        this.data$ = this.id$.pipe(
+            switchMap(id => this.agentQuery.watch({ id }).valueChanges),
+            map(result => result.data),
+        );
+    }
 }
