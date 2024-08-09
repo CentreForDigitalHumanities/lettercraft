ba9fd81b95a916cb21acb28e3030582aad9e916b
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2024-08-09 09:19:06 +0200

    Clean home component

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,45 +1,8 @@
-import { Component, OnInit } from "@angular/core";
-import { FormControl } from "@angular/forms";
-import { MultiselectItem } from "../data-entry/shared/multiselect/multiselect.component";
-
-const ITEMS: MultiselectItem[] = [
-    {
-        id: "1",
-        label: "Writing",
-    },
-    {
-        id: "2",
-        label: "Receiving",
-    },
-    {
-        id: "3",
-        label: "Sending",
-    },
-    {
-        id: "4",
-        label: "Licking",
-    },
-];
+import { Component } from "@angular/core";
 
 @Component({
     selector: "lc-home",
     templateUrl: "./home.component.html",
     styleUrls: ["./home.component.scss"],
 })
-export class HomeComponent implements OnInit {
-    public control = new FormControl<string[]>([], { nonNullable: true });
-
-    items = ITEMS;
-
-    ngOnInit(): void {
-        this.control.setValue(["1", "3"]);
-
-        this.control.valueChanges.subscribe((value) =>
-            console.log("New value:", value)
-        );
-    }
-
-    test(): void {
-        this.control.setValue(["2"])
-    }
-}
+export class HomeComponent {}
