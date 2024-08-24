db5b6786b9fed71b8571d6e7841ef9037f6ede14
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2024-08-24 10:57:38 +0200

    Simplify tests

--- backend/core/tests/test_contributors.py
+++ backend/core/tests/test_contributors.py
@@ -40,17 +40,9 @@ class ContributorTestCase(GraphQLTestCase):
             query=self.UPDATE_EPISODE_MUTATION,
             input_data={"id": self.episode.pk, "name": "Palpatine"},
         )
-
         self.assertResponseNoErrors(response)
 
-        content = json.loads(response.content)
-
-        self.assertTrue(content["data"]["updateEpisode"]["ok"])
-        self.assertEqual(len(content["data"]["updateEpisode"]["errors"]), 0)
-
         episode = Episode.objects.get(id=self.episode.pk)
-
-        self.assertEqual(episode.name, "Palpatine")
 
         first_contributor = episode.contributors.first()
 
@@ -66,34 +58,16 @@ class ContributorTestCase(GraphQLTestCase):
             query=self.UPDATE_EPISODE_MUTATION,
             input_data={"id": self.episode.pk, "name": "Palpatine"},
         )
-
         self.assertResponseNoErrors(response)
-
-        content = json.loads(response.content)
-
-        self.assertTrue(content["data"]["updateEpisode"]["ok"])
-        self.assertEqual(len(content["data"]["updateEpisode"]["errors"]), 0)
-
-        episode = Episode.objects.get(id=self.episode.pk)
-
-        self.assertEqual(episode.name, "Palpatine")
-        self.assertEqual(len(episode.contributors.all()), 1)
 
         response = self.query(
             query=self.UPDATE_EPISODE_MUTATION,
             input_data={"id": self.episode.pk, "name": "Darth Vader"},
         )
-
         self.assertResponseNoErrors(response)
-
-        content = json.loads(response.content)
-
-        self.assertTrue(content["data"]["updateEpisode"]["ok"])
-        self.assertEqual(len(content["data"]["updateEpisode"]["errors"]), 0)
 
         episode = Episode.objects.get(id=self.episode.pk)
 
-        self.assertEqual(episode.name, "Darth Vader")
         self.assertEqual(
             len(episode.contributors.all()),
             1,
@@ -102,42 +76,20 @@ class ContributorTestCase(GraphQLTestCase):
 
     def test_multiple_contributors_one_contribution(self):
         self.client.force_login(self.user_1)
-
         response = self.query(
             query=self.UPDATE_EPISODE_MUTATION,
             input_data={"id": self.episode.pk, "name": "Han"},
         )
-
         self.assertResponseNoErrors(response)
 
-        content = json.loads(response.content)
-
-        self.assertTrue(content["data"]["updateEpisode"]["ok"])
-        self.assertEqual(len(content["data"]["updateEpisode"]["errors"]), 0)
-
-        episode = Episode.objects.get(id=self.episode.pk)
-
-        self.assertEqual(episode.name, "Han")
-        self.assertEqual(len(episode.contributors.all()), 1)
-
         self.client.force_login(self.user_2)
-
         response = self.query(
             query=self.UPDATE_EPISODE_MUTATION,
             input_data={"id": self.episode.pk, "name": "Leia"},
         )
-
         self.assertResponseNoErrors(response)
 
-        content = json.loads(response.content)
-
-        self.assertTrue(content["data"]["updateEpisode"]["ok"])
-        self.assertEqual(len(content["data"]["updateEpisode"]["errors"]), 0)
-
         episode = Episode.objects.get(id=self.episode.pk)
-
-        self.assertEqual(episode.name, "Leia")
-        self.assertEqual(len(episode.contributors.all()), 2)
 
         first_contributor = episode.contributors.first()
         second_contributor = episode.contributors.last()
@@ -150,84 +102,31 @@ class ContributorTestCase(GraphQLTestCase):
 
     def test_multiple_contributors_multiple_contributions(self):
         self.client.force_login(self.user_1)
-
-        # First contribution by user 1
-        response = self.query(
+        response_1_1 = self.query(
             query=self.UPDATE_EPISODE_MUTATION,
             input_data={"id": self.episode.pk, "name": "Jabba"},
         )
-
-        self.assertResponseNoErrors(response)
-
-        content = json.loads(response.content)
-
-        self.assertTrue(content["data"]["updateEpisode"]["ok"])
-        self.assertEqual(len(content["data"]["updateEpisode"]["errors"]), 0)
-
-        episode = Episode.objects.get(id=self.episode.pk)
-
-        self.assertEqual(episode.name, "Jabba")
-        self.assertEqual(len(episode.contributors.all()), 1)
+        response_1_2 = self.query(
+            query=self.UPDATE_EPISODE_MUTATION,
+            input_data={"id": self.episode.pk, "name": "Anakin"},
+        )
 
         self.client.force_login(self.user_2)
-
-        # First contribution by user 2
-        response = self.query(
+        response_2_1 = self.query(
             query=self.UPDATE_EPISODE_MUTATION,
             input_data={"id": self.episode.pk, "name": "Jango"},
         )
-
-        self.assertResponseNoErrors(response)
-
-        content = json.loads(response.content)
-
-        self.assertTrue(content["data"]["updateEpisode"]["ok"])
-        self.assertEqual(len(content["data"]["updateEpisode"]["errors"]), 0)
-
-        episode = Episode.objects.get(id=self.episode.pk)
-
-        self.assertEqual(episode.name, "Jango")
-        self.assertEqual(len(episode.contributors.all()), 2)
-
-        self.client.force_login(self.user_1)
-
-        # Second contribution by user 1
-        response = self.query(
+        response_2_2 = self.query(
             query=self.UPDATE_EPISODE_MUTATION,
-            input_data={"id": self.episode.pk, "name": "Luke"},
+            input_data={"id": self.episode.pk, "name": "Boba"},
         )
 
-        self.assertResponseNoErrors(response)
-
-        content = json.loads(response.content)
-
-        self.assertTrue(content["data"]["updateEpisode"]["ok"])
-        self.assertEqual(len(content["data"]["updateEpisode"]["errors"]), 0)
+        self.assertResponseNoErrors(response_1_1)
+        self.assertResponseNoErrors(response_1_2)
+        self.assertResponseNoErrors(response_2_1)
+        self.assertResponseNoErrors(response_2_2)
 
         episode = Episode.objects.get(id=self.episode.pk)
-
-        self.assertEqual(episode.name, "Luke")
-        self.assertEqual(len(episode.contributors.all()), 2)
-
-        self.client.force_login(self.user_2)
-
-        # Second contribution by user 2
-        response = self.query(
-            query=self.UPDATE_EPISODE_MUTATION,
-            input_data={"id": self.episode.pk, "name": "Jar-Jar"},
-        )
-
-        self.assertResponseNoErrors(response)
-
-        content = json.loads(response.content)
-
-        self.assertTrue(content["data"]["updateEpisode"]["ok"])
-        self.assertEqual(len(content["data"]["updateEpisode"]["errors"]), 0)
-
-        episode = Episode.objects.get(id=self.episode.pk)
-
-        self.assertEqual(episode.name, "Jar-Jar")
-        self.assertEqual(len(episode.contributors.all()), 2)
 
         first_contributor = episode.contributors.first()
         second_contributor = episode.contributors.last()
