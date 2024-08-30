41dded2c8b76facdae9418b28cc333bd4d8e308a
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2024-08-30 16:09:55 +0200

    Update base letter form

--- frontend/src/app/data-entry/letter-form/letter-form.component.ts
+++ frontend/src/app/data-entry/letter-form/letter-form.component.ts
@@ -1,52 +1,56 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Breadcrumb } from '@shared/breadcrumb/breadcrumb.component';
-import { dataIcons } from '@shared/icons';
-import { DataEntryLetterGQL, DataEntryLetterQuery } from 'generated/graphql';
-import { map, Observable, switchMap } from 'rxjs';
+import { Component } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Breadcrumb } from "@shared/breadcrumb/breadcrumb.component";
+import { actionIcons, dataIcons } from "@shared/icons";
+import { DataEntryLetterFormGQL } from "generated/graphql";
+import { filter, map, Observable, share, switchMap } from "rxjs";
 
 @Component({
-    selector: 'lc-letter-form',
-    templateUrl: './letter-form.component.html',
-    styleUrls: ['./letter-form.component.scss']
+    selector: "lc-letter-form",
+    templateUrl: "./letter-form.component.html",
+    styleUrls: ["./letter-form.component.scss"],
 })
 export class LetterFormComponent {
-    id$: Observable<string>;
-    data$: Observable<DataEntryLetterQuery>;
+    private id$ = this.route.params.pipe(map((params) => params["id"]));
 
-    dataIcons = dataIcons;
+    public letter$ = this.id$.pipe(
+        switchMap((id) => this.letterQuery.watch({ id }).valueChanges),
+        map((result) => result.data.letterDescription),
+        share()
+    );
 
-    constructor(private route: ActivatedRoute, private letterQuery: DataEntryLetterGQL) {
-        this.id$ = this.route.params.pipe(
-            map(params => params['id']),
-        );
-        this.data$ = this.id$.pipe(
-            switchMap(id => this.letterQuery.watch({ id }).valueChanges),
-            map(result => result.data),
-        );
-    }
-
-    getBreadcrumbs(data: DataEntryLetterQuery): Breadcrumb[] {
-        if (data.letterDescription) {
+    public breadcrumbs$ = this.letter$.pipe(
+        filter((letter) => !!letter),
+        map((letter) => {
+            if (!letter) {
+                return [];
+            }
             return [
-                { link: '/', label: 'Lettercraft' },
-                { link: '/data-entry', label: 'Data entry' },
                 {
-                    link: `/data-entry/sources/${data.letterDescription.source.id}`,
-                    label: data.letterDescription.source.name
+                    label: "Lettercraft",
+                    link: "/",
                 },
                 {
-                    link: `/data-entry/letters/${data.letterDescription.id}`,
-                    label: data.letterDescription.name
+                    label: "Data entry",
+                    link: "/data-entry",
+                },
+                {
+                    label: letter.source.name,
+                    link: `/source/${letter.source.id}`,
+                },
+                {
+                    label: letter.name,
+                    link: `/data-entry/letters/${letter.id}`,
                 },
             ];
-        } else {
-            return [
-                { link: '/', label: 'Lettercraft' },
-                { link: '/data-entry', label: 'Data entry' },
-                { link: '', label: 'Letter not found' }
-            ]
-        }
-    }
+        })
+    );
 
+    public dataIcons = dataIcons;
+    public actionIcons = actionIcons;
+
+    constructor(
+        private route: ActivatedRoute,
+        private letterQuery: DataEntryLetterFormGQL
+    ) {}
 }
