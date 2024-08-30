313c42149a1bb6171f1809446113551c5237531e
Luka van der Plas <lukavdplas@mailfence.com>
2024-08-30 12:29:24 +0200

    fetch data in agent episodes form

--- frontend/src/app/data-entry/agent-form/agent-episodes-form/agent-episodes-form.component.ts
+++ frontend/src/app/data-entry/agent-form/agent-episodes-form/agent-episodes-form.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormService } from '../../shared/form.service';
+import { DataEntryAgentEpisodesGQL, DataEntryAgentEpisodesQuery } from 'generated/graphql';
+import { map, Observable, switchMap } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'lc-agent-episodes-form',
@@ -6,5 +10,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./agent-episodes-form.component.scss']
 })
 export class AgentEpisodesFormComponent {
+    data$: Observable<DataEntryAgentEpisodesQuery>;
 
+    constructor(
+        private formService: FormService,
+        private query: DataEntryAgentEpisodesGQL,
+    ) {
+        this.data$ = this.formService.id$.pipe(
+            switchMap(id => this.query.watch({ id }).valueChanges),
+            map(result => result.data),
+            takeUntilDestroyed(),
+        );
+    }
 }
