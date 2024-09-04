83c28ad7d2921bc5be00f419f1e4f0ce81773082
Luka van der Plas <lukavdplas@mailfence.com>
2024-09-04 15:32:58 +0200

    sync form status of episode-link forms

--- frontend/src/app/data-entry/shared/utils.ts
+++ frontend/src/app/data-entry/shared/utils.ts
@@ -1,7 +1,10 @@
 import { SourceMention } from "generated/graphql";
-import { SelectOptions } from "./types";
+import { FormStatus, SelectOptions } from "./types";
+import { BehaviorSubject } from "rxjs";
 
 export const sourceMentionSelectOptions = (): SelectOptions<SourceMention> => [
     { value: SourceMention.Direct, label: 'Directly mentioned' },
     { value: SourceMention.Implied, label: 'Implied' }
 ];
+
+export const formStatusSubject = () => new BehaviorSubject<FormStatus>('idle');
