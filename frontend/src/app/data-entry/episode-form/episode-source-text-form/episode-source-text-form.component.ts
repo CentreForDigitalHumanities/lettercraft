15315911632f3e841f29616f94cbc6a4497411a6
Luka van der Plas <lukavdplas@mailfence.com>
2024-09-09 17:20:08 +0200

    show status in source text fields

--- frontend/src/app/data-entry/episode-form/episode-source-text-form/episode-source-text-form.component.ts
+++ frontend/src/app/data-entry/episode-form/episode-source-text-form/episode-source-text-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, OnInit } from "@angular/core";
+import { Component, DestroyRef, OnDestroy, OnInit } from "@angular/core";
 import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 import { FormControl, FormGroup } from "@angular/forms";
 import { ActivatedRoute } from "@angular/router";
@@ -6,6 +6,7 @@ import { ToastService } from "@services/toast.service";
 import {
     DataEntryEpisodeSourceTextMentionGQL,
     DataEntryUpdateEpisodeGQL,
+    DataEntryUpdateEpisodeMutation,
 } from "generated/graphql";
 import {
     debounceTime,
@@ -14,15 +15,19 @@ import {
     Observable,
     shareReplay,
     switchMap,
+    tap,
     withLatestFrom,
 } from "rxjs";
+import { FormService } from "../../shared/form.service";
+import { formStatusSubject } from "../../shared/utils";
+import { MutationResult } from "apollo-angular";
 
 @Component({
     selector: "lc-episode-source-text-form",
     templateUrl: "./episode-source-text-form.component.html",
     styleUrls: ["./episode-source-text-form.component.scss"],
 })
-export class EpisodeSourceTextFormComponent implements OnInit {
+export class EpisodeSourceTextFormComponent implements OnInit, OnDestroy {
     private id$: Observable<string> = this.route.params.pipe(
         map((params) => params["id"])
     );
@@ -40,13 +45,18 @@ export class EpisodeSourceTextFormComponent implements OnInit {
         page: new FormControl<string>("", { nonNullable: true }),
     });
 
+    private status$ = formStatusSubject();
+
     constructor(
         private destroyRef: DestroyRef,
         private route: ActivatedRoute,
         private toastService: ToastService,
         private episodeQuery: DataEntryEpisodeSourceTextMentionGQL,
-        private updateEpisode: DataEntryUpdateEpisodeGQL
-    ) {}
+        private updateEpisode: DataEntryUpdateEpisodeGQL,
+        private formService: FormService,
+    ) {
+        this.formService.attachForm('source-text', this.status$);
+    }
 
     ngOnInit(): void {
         this.episode$
@@ -63,33 +73,40 @@ export class EpisodeSourceTextFormComponent implements OnInit {
 
         this.episode$
             .pipe(
-                switchMap(() =>
-                    this.form.valueChanges.pipe(
-                        map(() => this.form.getRawValue()),
-                        filter(() => this.form.valid),
-                        debounceTime(300),
-                        withLatestFrom(this.id$),
-                        switchMap(([episode, id]) =>
-                            this.updateEpisode.mutate({
-                                episodeData: {
-                                    id,
-                                    ...episode,
-                                },
-                            })
-                        ),
-                        takeUntilDestroyed(this.destroyRef)
-                    )
-                )
-            )
-            .subscribe((result) => {
-                const errors = result.data?.updateEpisode?.errors;
-                if (errors && errors.length > 0) {
-                    this.toastService.show({
-                        body: errors.map((error) => error.messages).join("\n"),
-                        type: "danger",
-                        header: "Update failed",
-                    });
-                }
+                switchMap(() => this.form.valueChanges),
+                map(() => this.form.getRawValue()),
+                filter(() => this.form.valid),
+                debounceTime(300),
+                withLatestFrom(this.id$),
+                tap(() => this.status$.next('loading')),
+                switchMap(([episode, id]) =>
+                    this.updateEpisode.mutate({
+                        episodeData: {
+                            id,
+                            ...episode,
+                        },
+                    })
+                ),
+                takeUntilDestroyed(this.destroyRef)
+        )
+            .subscribe(this.handleResult.bind(this));
+    }
+
+    ngOnDestroy(): void {
+        this.formService.detachForm('source-text');
+    }
+
+    private handleResult(result: MutationResult<DataEntryUpdateEpisodeMutation>) {
+        const errors = result.data?.updateEpisode?.errors;
+        if (errors && errors.length > 0) {
+            this.status$.next('error');
+            this.toastService.show({
+                body: errors.map((error) => error.messages).join("\n"),
+                type: "danger",
+                header: "Update failed",
             });
+        } else {
+            this.status$.next('saved');
+        }
     }
 }
