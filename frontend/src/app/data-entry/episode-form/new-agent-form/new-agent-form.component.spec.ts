5dcc206d14484f4133ae73af00941aeef4f81a2a
Luka van der Plas <lukavdplas@mailfence.com>
2024-09-10 15:28:17 +0200

    update new agent form spec

--- frontend/src/app/data-entry/episode-form/new-agent-form/new-agent-form.component.spec.ts
+++ frontend/src/app/data-entry/episode-form/new-agent-form/new-agent-form.component.spec.ts
@@ -1,21 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NewAgentFormComponent } from './new-agent-form.component';
+import { SharedTestingModule } from '@shared/shared-testing.module';
 
 describe('NewAgentFormComponent', () => {
-  let component: NewAgentFormComponent;
-  let fixture: ComponentFixture<NewAgentFormComponent>;
+    let component: NewAgentFormComponent;
+    let fixture: ComponentFixture<NewAgentFormComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [NewAgentFormComponent]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [NewAgentFormComponent],
+            imports: [SharedTestingModule],
+        });
+        fixture = TestBed.createComponent(NewAgentFormComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(NewAgentFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
