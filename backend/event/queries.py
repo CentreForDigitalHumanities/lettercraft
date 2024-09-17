1e9cf6b02cb884611c3e25e6f1c76824409c721c
Luka van der Plas <lukavdplas@mailfence.com>
2024-09-17 13:53:01 +0200

    remove unused query

--- backend/event/queries.py
+++ backend/event/queries.py
@@ -1,9 +1,8 @@
 from graphene import ID, Field, List, NonNull, ObjectType, ResolveInfo
 from django.db.models import QuerySet, Q
-from event.models import Episode, EpisodeCategory, EpisodeEntity, EpisodeAgent
+from event.models import Episode, EpisodeCategory, EpisodeEntity
 from event.types.EpisodeCategoryType import EpisodeCategoryType
 from event.types.EpisodeType import EpisodeType
-from event.types.EpisodeAgentType import EpisodeAgentType
 from event.types.EpisodeEntityLink import EpisodeEntityLink
 from core.types.entity import Entity
 from core.entity_models import ENTITY_MODELS
@@ -13,9 +12,6 @@ class EventQueries(ObjectType):
     episode = Field(EpisodeType, id=ID(required=True))
     episodes = List(NonNull(EpisodeType), required=True, source_id=ID())
     episode_categories = List(NonNull(EpisodeCategoryType), required=True)
-    episode_agent_link = Field(
-        EpisodeAgentType, agent=ID(required=True), episode=ID(required=True)
-    )
     episode_entity_link = Field(
         EpisodeEntityLink,
         entity=ID(required=True),
@@ -45,20 +41,6 @@ class EventQueries(ObjectType):
         parent: None, info: ResolveInfo
     ) -> QuerySet[EpisodeCategory]:
         return EpisodeCategoryType.get_queryset(EpisodeCategory.objects, info).all()
-
-    @staticmethod
-    def resolve_episode_agent_link(
-        parent: None,
-        info: ResolveInfo,
-        agent: str,
-        episode: str,
-    ) -> EpisodeAgent | None:
-        try:
-            return EpisodeAgentType.get_queryset(EpisodeAgent.objects, info).get(
-                agent__id=agent, episode__id=episode
-            )
-        except EpisodeAgent.DoesNotExist:
-            return None
 
     @staticmethod
     def resolve_episode_entity_link(
