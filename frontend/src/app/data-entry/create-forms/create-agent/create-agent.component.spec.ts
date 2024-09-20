267236c9b41b07a4c44e2c8d10c1e3b1bb26b409
Luka van der Plas <lukavdplas@mailfence.com>
2024-09-20 12:20:36 +0200

    update create agent form spec

--- frontend/src/app/data-entry/create-forms/create-agent/create-agent.component.spec.ts
+++ frontend/src/app/data-entry/create-forms/create-agent/create-agent.component.spec.ts
@@ -1,21 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreateAgentComponent } from './create-agent.component';
+import { SharedTestingModule } from '@shared/shared-testing.module';
+import { Subject } from 'rxjs';
 
 describe('CreateAgentComponent', () => {
-  let component: CreateAgentComponent;
-  let fixture: ComponentFixture<CreateAgentComponent>;
+    let component: CreateAgentComponent;
+    let fixture: ComponentFixture<CreateAgentComponent>;
+    let create$: Subject<void>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [CreateAgentComponent]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [CreateAgentComponent],
+            imports: [SharedTestingModule],
+        });
+        fixture = TestBed.createComponent(CreateAgentComponent);
+        component = fixture.componentInstance;
+        create$ = new Subject<void>();
+        component.create = create$;
+        component.sourceID = '1';
+        fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(CreateAgentComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should open a modal', () => {
+        create$.next();
+        expect(component.modal).toBeTruthy();
+    })
 });
