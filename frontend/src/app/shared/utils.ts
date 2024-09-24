f034b6c08f7b28554ddd15bea7f4990e4e482eda
Luka van der Plas <lukavdplas@mailfence.com>
2024-09-24 14:37:08 +0200

    update docstrings

--- frontend/src/app/shared/utils.ts
+++ frontend/src/app/shared/utils.ts
@@ -7,7 +7,7 @@ import _ from "underscore";
  * Similar to _.difference, but compares on a specified key.
  *
  * @param list The starting list
- * @param others The list with values to excluded
+ * @param others The list with values to exclude
  * @param key The property by which to compare objects
  * @returns The contents of `list` for which no value in `others` matches the `key`
  * property.
@@ -26,11 +26,11 @@ export const differenceBy = <T extends object>(
  * For example:
  *
  * ```ts
- * splat(Math.min)([3, 5, 2]) // => 2
+ * splat(Math.min)([3, 5, 2]) // => Math.min(3, 5, 2) => 2
  * ```
  *
  * @param func Function to transform
- * @return A function that accepts an array of the arguments to `func`, and returns
+ * @returns A function that accepts an array of the arguments to `func`, and returns
  * the output of `func`.
  */
 export const splat = <I, O>(func: (...a: I[]) => O) =>
