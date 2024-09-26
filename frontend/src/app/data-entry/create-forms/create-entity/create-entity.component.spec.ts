c0780c3bd56a1a6b33639adfaf2de5ee52a18237
Luka van der Plas <lukavdplas@mailfence.com>
2024-09-26 12:14:29 +0200

    fix test module

--- frontend/src/app/data-entry/create-forms/create-entity/create-entity.component.spec.ts
+++ frontend/src/app/data-entry/create-forms/create-entity/create-entity.component.spec.ts
@@ -3,7 +3,11 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CreateEntityComponent } from './create-entity.component';
 import { SharedTestingModule } from '@shared/shared-testing.module';
 import { Subject } from 'rxjs';
+import { CreateEntityService } from './create-entity.service';
 import { CreateAgentService } from './create-agent.service';
+import { CreateGiftService } from './create-gift.service';
+import { CreateLetterService } from './create-letter.service';
+import { CreateSpaceService } from './create-space.service';
 
 describe('CreateAgentComponent', () => {
     let component: CreateEntityComponent;
@@ -14,7 +18,8 @@ describe('CreateAgentComponent', () => {
         TestBed.configureTestingModule({
             declarations: [CreateEntityComponent],
             imports: [SharedTestingModule],
-            providers: [CreateAgentService],
+            providers: [CreateEntityService, CreateAgentService, CreateGiftService,
+                CreateLetterService, CreateSpaceService],
         });
         fixture = TestBed.createComponent(CreateEntityComponent);
         component = fixture.componentInstance;
