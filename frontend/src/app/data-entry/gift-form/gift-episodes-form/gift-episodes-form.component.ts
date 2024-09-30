0d90eb2580c499df3c98cd5910d65dab690f30c1
Luka van der Plas <lukavdplas@mailfence.com>
2024-09-30 16:52:41 +0200

    generate gift episodes form component

---
+++ frontend/src/app/data-entry/gift-form/gift-episodes-form/gift-episodes-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lc-gift-episodes-form',
+  templateUrl: './gift-episodes-form.component.html',
+  styleUrls: ['./gift-episodes-form.component.scss']
+})
+export class GiftEpisodesFormComponent {
+
+}
