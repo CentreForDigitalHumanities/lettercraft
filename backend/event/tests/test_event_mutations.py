825f09b2475702163d0f1120f2292d6d579addf1
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2024-10-30 16:51:34 +0100

    Add backend test

---
+++ backend/event/tests/test_event_mutations.py
@@ -0,0 +1,29 @@
+from django.db.models.query import QuerySet
+
+
+def test_episode_order_mutation(graphql_client, episode, episode_2):
+    result = graphql_client.execute(
+        f"""
+        mutation TestMutation {{
+            updateEpisodeOrder(
+                episodeIds: [
+                    {episode_2.pk}, {episode.pk}
+                ]
+            ) {{
+                ok
+                errors {{
+                    field
+                    messages
+                }}
+            }}
+        }}
+        """
+    )
+
+    assert result["data"]["updateEpisodeOrder"]["ok"] == True
+    assert result["data"]["updateEpisodeOrder"]["errors"] == []
+
+    order_queryset = episode.source.get_episode_order() # type: QuerySet
+    ids_list = list(order_queryset.values_list("id", flat=True))
+
+    assert ids_list == [episode_2.pk, episode.pk]
