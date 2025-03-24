4335bc828e7cfdcbbce59532afab49df3b8b2561
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2025-03-24 11:52:39 +0100

    Generate Schema in GitHub workflow

--- frontend/codegen.ts
+++ frontend/codegen.ts
@@ -1,8 +1,18 @@
 import { environment } from './src/environments/environment';
 import type { CodegenConfig } from '@graphql-codegen/cli'
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+let schema = `http://localhost:8000${environment.graphqlUrl}`;
+
+// For GitHub workflows.
+if ("CODEGEN_SCHEMA" in process.env && process.env['CODEGEN_SCHEMA']) {
+    schema = process.env['CODEGEN_SCHEMA'];
+}
 
 const config: CodegenConfig = {
-    schema: `http://localhost:8000${environment.graphqlUrl}`,
+    schema,
     generates: {
         './generated/graphql.ts': {
             plugins: [
