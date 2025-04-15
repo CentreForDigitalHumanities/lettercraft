8e55d50d0f42415e6628b2cf822ec59d080576ca
Luka van der Plas <lukavdplas@mailfence.com>
2025-04-15 14:21:43 +0200

    reuse episode-links component in episode view

--- frontend/src/app/data/shared/episode-links/episode-links.component.ts
+++ frontend/src/app/data/shared/episode-links/episode-links.component.ts
@@ -1,13 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { dataIcons } from '@shared/icons';
+import { agentIcon, locationIcon } from '@shared/icons-utils';
 import { sourceMentionLabels } from '@shared/labels';
-import { SourceMention, ViewAgentQuery, ViewGiftQuery, ViewLetterQuery, ViewLocationQuery } from 'generated/graphql';
+import { SourceMention, ViewAgentQuery, ViewEpisodeQuery, ViewGiftQuery, ViewLetterQuery, ViewLocationQuery } from 'generated/graphql';
 
 type QueriedEpisodeLink =
     | NonNullable<ViewAgentQuery["agentDescription"]>["episodes"][number]
     | NonNullable<ViewLetterQuery["letterDescription"]>["episodes"][number]
     | NonNullable<ViewGiftQuery["giftDescription"]>["episodes"][number]
-    | NonNullable<ViewLocationQuery["spaceDescription"]>["episodes"][number];
+    | NonNullable<ViewLocationQuery["spaceDescription"]>["episodes"][number]
+    | NonNullable<ViewEpisodeQuery['episode']>['agents'|'gifts'|'letters'|'spaces'][number];
+
+type LinkedObject = { id: string, name: string, urlSegment: string, icon: string };
 
 @Component({
   selector: 'lc-episode-links',
@@ -16,10 +20,44 @@ type QueriedEpisodeLink =
 })
 export class EpisodeLinksComponent {
     @Input({required: true})
-    episodes!: QueriedEpisodeLink[];
+    data!: QueriedEpisodeLink[];
 
     dataIcons = dataIcons;
 
     SourceMention = SourceMention;
     sourceMentionLabels = sourceMentionLabels;
+
+    linkedObject(link: QueriedEpisodeLink): LinkedObject | undefined {
+        if ('episode' in link) {
+            return {
+                ...link.episode,
+                urlSegment: 'data/episodes',
+                icon: dataIcons.episode,
+            };
+        } else if ('agent' in link) {
+            return {
+                ...link.agent,
+                urlSegment: 'data/agents',
+                icon: agentIcon(link.agent),
+            };
+        } else if ('gift' in link) {
+            return {
+                ...link.gift,
+                urlSegment: 'data/gifts',
+                icon: dataIcons.gift,
+            };
+        } else if ('letter' in link) {
+            return {
+                ...link.letter,
+                urlSegment: 'data/letters',
+                icon: dataIcons.letter,
+            };
+        } else {
+            return {
+                ...link.space,
+                urlSegment: 'data/locations',
+                icon: locationIcon(link.space),
+            };
+        }
+    }
 }
