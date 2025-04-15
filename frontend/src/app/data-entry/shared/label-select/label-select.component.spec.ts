4a7c8b047263d223ae3418cc476ddb6628956dcd
Luka van der Plas <lukavdplas@mailfence.com>
2025-04-15 16:05:52 +0200

    update spec

--- frontend/src/app/data-entry/shared/label-select/label-select.component.spec.ts
+++ frontend/src/app/data-entry/shared/label-select/label-select.component.spec.ts
@@ -1,21 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LabelSelectComponent } from './label-select.component';
+import { SharedTestingModule } from '@shared/shared-testing.module';
 
 describe('LabelSelectComponent', () => {
-  let component: LabelSelectComponent;
-  let fixture: ComponentFixture<LabelSelectComponent>;
+    let component: LabelSelectComponent;
+    let fixture: ComponentFixture<LabelSelectComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [LabelSelectComponent]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [LabelSelectComponent],
+            imports: [SharedTestingModule],
+        });
+        fixture = TestBed.createComponent(LabelSelectComponent);
+        component = fixture.componentInstance;
+        component.labels = [
+            { value: '1', label: 'Test' },
+            { value: '2', label: 'Another test' },
+        ];
+        fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(LabelSelectComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
