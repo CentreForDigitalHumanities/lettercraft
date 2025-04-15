37f5de2a80367dfabc101d46d744cec151043063
Luka van der Plas <lukavdplas@mailfence.com>
2025-04-15 15:43:07 +0200

    implement formvalueaccessor in label select

--- frontend/src/app/data-entry/shared/label-select/label-select.component.ts
+++ frontend/src/app/data-entry/shared/label-select/label-select.component.ts
@@ -1,26 +1,55 @@
-import { Component, Input } from '@angular/core';
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
-type Label=  { value: string, label: string };
+type Label = { value: string, label: string };
 
 @Component({
-  selector: 'lc-label-select',
-  templateUrl: './label-select.component.html',
-  styleUrls: ['./label-select.component.scss']
+    selector: 'lc-label-select',
+    templateUrl: './label-select.component.html',
+    styleUrls: ['./label-select.component.scss'],
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => LabelSelectComponent),
+            multi: true,
+        },
+    ],
 })
-export class LabelSelectComponent {
-    @Input({required: true}) labels!: Label[];
+export class LabelSelectComponent implements ControlValueAccessor {
+    @Input({ required: true }) labels!: Label[];
 
-    selection: string[] = [];
+    value: string[] = [];
+
+    private onChange?: (value: string[]) => any;
+    private onTouched?: () => any;
 
     isSelected(label: Label): boolean {
-        return this.selection.includes(label.value);
+        return this.value.includes(label.value);
     }
 
     toggle(label: Label): void {
         if (this.isSelected(label)) {
-            this.selection = this.selection.filter(i => i !== label.value);
+            this.value = this.value.filter(i => i !== label.value);
         } else {
-            this.selection.push(label.value);
+            this.value.push(label.value);
         }
+        this.onChange?.(this.value);
+    }
+
+    blur() {
+        this.onTouched?.();
+    }
+
+    writeValue(obj: string[]): void {
+        this.value = obj;
+        this.onChange?.(this.value);
+    }
+
+    registerOnChange(fn: () => any): void {
+        this.onChange = fn;
+    }
+
+    registerOnTouched(fn: () => any): void {
+        this.onTouched = fn;
     }
 }
