357903080521be9bff0b1910f69033aed47b8f82
Luka van der Plas <lukavdplas@mailfence.com>
2025-04-15 15:16:31 +0200

    show state in label-select

--- frontend/src/app/data-entry/shared/label-select/label-select.component.ts
+++ frontend/src/app/data-entry/shared/label-select/label-select.component.ts
@@ -1,10 +1,26 @@
 import { Component, Input } from '@angular/core';
 
+type Label=  { value: string, label: string };
+
 @Component({
   selector: 'lc-label-select',
   templateUrl: './label-select.component.html',
   styleUrls: ['./label-select.component.scss']
 })
 export class LabelSelectComponent {
-    @Input({required: true}) labels!: { label: string }[];
+    @Input({required: true}) labels!: Label[];
+
+    selection: string[] = [];
+
+    isSelected(label: Label): boolean {
+        return this.selection.includes(label.value);
+    }
+
+    toggle(label: Label): void {
+        if (this.isSelected(label)) {
+            this.selection = this.selection.filter(i => i !== label.value);
+        } else {
+            this.selection.push(label.value);
+        }
+    }
 }
