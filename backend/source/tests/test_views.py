70c193fe3eb9ee4de8d00a4a50f4fceebadc780d
Luka van der Plas <lukavdplas@mailfence.com>
2025-07-08 15:06:32 +0200

    add checks to image view

--- backend/source/tests/test_views.py
+++ backend/source/tests/test_views.py
@@ -3,7 +3,7 @@ import pytest
 from django.core.files.images import ImageFile
 from rest_framework import status
 
-from source.models import SourceImage
+from source.models import SourceImage, Source
 
 here = os.path.dirname(os.path.abspath(__file__))
 
@@ -22,8 +22,22 @@ def source_image(source) -> SourceImage:
 
     return instance
 
+
 def test_source_image_view(source_image: SourceImage, client):
     pk = source_image.source.pk
-
     response = client.get(f'/api/source-images/{pk}')
     assert response.status_code == status.HTTP_200_OK
+
+
+def test_source_image_view_not_found(source: Source, client):
+    response = client.get(f'/api/source-images/{source.pk}')
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+def test_source_image_view_private(source_image: SourceImage, client):
+    source = source_image.source
+    source.is_public = False
+    source.save()
+
+    response = client.get(f'/api/source-images/{source.pk}')
+    assert response.status_code == status.HTTP_404_NOT_FOUND
--- backend/source/views.py
+++ backend/source/views.py
@@ -1,11 +1,17 @@
 from rest_framework.views import APIView
 from django.http.response import FileResponse
+from rest_framework.exceptions import NotFound
 
 from source.models import SourceImage
 
 class SourceImageView(APIView):
     def get(self, request, *args, **kwargs):
         pk = self.kwargs.get('pk')
-        instance = SourceImage.objects.get(source__pk=pk)
+
+        try:
+            instance = SourceImage.objects.get(source__pk=pk, source__is_public=True)
+        except SourceImage.DoesNotExist:
+            raise NotFound('Image not found')
+
         path = instance.image.path
         return FileResponse(open(path, 'rb'))
