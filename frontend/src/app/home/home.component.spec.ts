8557d2f6865b9d6bd98fb5e533cfe13e367a7929
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2025-08-22 11:17:15 +0200

    Fix failing tests

--- frontend/src/app/home/home.component.spec.ts
+++ frontend/src/app/home/home.component.spec.ts
@@ -1,18 +1,19 @@
 import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { HomeComponent } from './home.component';
+import { SharedModule } from '@shared/shared.module';
 
 describe('HomeComponent', () => {
     let component: HomeComponent;
     let fixture: ComponentFixture<HomeComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ HomeComponent ],
-      imports: [ HttpClientTestingModule ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(waitForAsync(() => {
+        TestBed.configureTestingModule({
+            declarations: [HomeComponent],
+            imports: [HttpClientTestingModule, SharedModule]
+        })
+            .compileComponents();
+    }));
 
     beforeEach(() => {
         fixture = TestBed.createComponent(HomeComponent);
