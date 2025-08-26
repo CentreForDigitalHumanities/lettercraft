472d9fc46e26ee7acce6327aa66cf5ae57c91cb0
Luka van der Plas <lukavdplas@mailfence.com>
2025-08-26 15:59:50 +0200

    paginated episodes in source view

--- frontend/src/app/data/source-view/source-view.component.ts
+++ frontend/src/app/data/source-view/source-view.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { actionIcons, dataIcons } from '@shared/icons';
 import { agentIcon, locationIcon } from '@shared/icons-utils';
-import { ViewSourceGQL } from 'generated/graphql';
-import { map, Observable, switchMap } from 'rxjs';
+import { ViewSourceEpisodesPageGQL, ViewSourceGQL } from 'generated/graphql';
+import { map, Observable, switchMap, filter } from 'rxjs';
 import { sourceBreadcrumbs } from '../utils/breadcrumbs';
+import { PageResult } from '../utils/pagination';
 
 @Component({
   selector: 'lc-source-view',
@@ -27,8 +28,19 @@ export class SourceViewComponent {
 
     makeBreadcrumbs = sourceBreadcrumbs;
 
+    episodesCollection$ = this.data$.pipe(
+        filter(data => !!data.source),
+        map(data => data.source?.episodes || []),
+    );
+
+    episodesPageResult = new PageResult(
+        this.episodesCollection$,
+        ids => this.episodesPageQuery.watch({ids}).valueChanges,
+    );
+
     constructor(
         private route: ActivatedRoute,
-        private query: ViewSourceGQL
+        private query: ViewSourceGQL,
+        private episodesPageQuery: ViewSourceEpisodesPageGQL,
     ) { }
 }
