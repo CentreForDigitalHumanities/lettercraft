0ec3fae9c4a25f6083d5b299faf7b41676e66b7e
Luka van der Plas <lukavdplas@mailfence.com>
2025-08-26 14:16:42 +0200

    fetch paged sources

--- frontend/src/app/data/source-list/source-list.component.ts
+++ frontend/src/app/data/source-list/source-list.component.ts
@@ -2,8 +2,10 @@ import { Component } from "@angular/core";
 import { FormControl } from "@angular/forms";
 import { SearchService } from "@services/search.service";
 import { Breadcrumb } from "@shared/breadcrumb/breadcrumb.component";
-import { ViewSourcesGQL, ViewSourcesQuery } from "generated/graphql";
-import { map, distinctUntilChanged, startWith, filter } from "rxjs";
+import { ViewSourcesGQL, ViewSourcesPageGQL, ViewSourcesQuery } from "generated/graphql";
+import { map, distinctUntilChanged, startWith, filter, switchMap, shareReplay, BehaviorSubject, combineLatest } from "rxjs";
+
+
 
 @Component({
     selector: "lc-source-list",
@@ -11,6 +13,8 @@ import { map, distinctUntilChanged, startWith, filter } from "rxjs";
     styleUrls: ["./source-list.component.scss"],
 })
 export class SourceListComponent {
+    pageSize = 10;
+
     breadcrumbs: Breadcrumb[] = [
         { link: "/", label: "Lettercraft" },
         { link: "/data", label: "Data" },
@@ -21,24 +25,54 @@ export class SourceListComponent {
         nonNullable: true,
     });
 
-    private search$ = this.searchService.createSearch<ViewSourcesQuery>(
+    private allSources$ = this.searchService.createSearch<ViewSourcesQuery>(
         this.searchControl.valueChanges,
         this.query
     );
 
-    public data$ = this.search$.pipe(
+    private ids$ = this.allSources$.pipe(
         filter((state) => !state.loading),
-        map((state) => state.data)
+        map((state) => state.data),
+        filter(data => !!data),
+        map(data => data?.sources.map(src => src.id)),
+        shareReplay(),
     );
 
-    public loading$ = this.search$.pipe(
+    public allLoading$ = this.allSources$.pipe(
         map((state) => state.loading),
         distinctUntilChanged(),
         startWith(false)
     );
 
+    totalSize$ = this.ids$.pipe(
+        map(ids => ids?.length),
+    );
+
+    private page$ = new BehaviorSubject<number>(0);
+
+    private pageResult$ = combineLatest([this.ids$, this.page$]).pipe(
+        map(([ids, page]) => ids ? this.slicePage(ids, page) : []),
+        switchMap(ids => this.pageQuery.watch({ ids }).result()),
+    )
+
+    public data$ = this.pageResult$.pipe(
+        filter((state) => !state.loading),
+        map((state) => state.data)
+    );
+
+
     constructor(
         private query: ViewSourcesGQL,
-        private searchService: SearchService
+        private searchService: SearchService,
+        private pageQuery: ViewSourcesPageGQL,
     ) {}
+
+    get page(): number { return this.page$.value }
+    set page(value: number) { this.page$.next(value) }
+
+    slicePage<T>(values: T[], page: number): T[] {
+        const start = this.pageSize * (page - 1);
+        const end = this.pageSize * page;
+        return values .slice(start, end);
+    }
 }
