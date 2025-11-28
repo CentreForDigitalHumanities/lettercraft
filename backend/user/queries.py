4d07a99f0c2efb613705711da36b45ec0c7fd50c
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2025-11-28 16:02:19 +0100

    Remove unused user queries (for now)

--- backend/user/queries.py
+++ backend/user/queries.py
@@ -1,29 +1,5 @@
-from graphene import Field, List, ResolveInfo
-from django.db.models import QuerySet
-from graphene import ID, Field, List, NonNull, ObjectType, ResolveInfo
-from django.db.models import QuerySet
-from typing import Optional
-
-from user.types.UserType import UserType
-from user.models import User
+from graphene import ObjectType
 
 
 class UserQueries(ObjectType):
-    user_description = Field(UserType, id=ID(required=True))
-    user_descriptions = List(NonNull(UserType), required=True)
-
-    @staticmethod
-    def resolve_user_description(
-        parent: None, info: ResolveInfo, id: str
-    ) -> Optional[User]:
-        try:
-            return UserType.get_queryset(User.objects, info).get(id=id)
-        except User.DoesNotExist:
-            return None
-
-    @staticmethod
-    def resolve_user_descriptions(
-        parent: None,
-        info: ResolveInfo,
-    ) -> QuerySet[User]:
-        return UserType.get_queryset(User.objects, info)
+    pass
