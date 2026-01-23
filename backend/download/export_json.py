93761ed5269e443603960b8c59630362fc4be283
Luka van der Plas <lukavdplas@mailfence.com>
2026-01-23 15:11:07 +0100

    basic export

--- backend/download/export_json.py
+++ backend/download/export_json.py
@@ -5,6 +5,10 @@ import json
 from django.db.models import QuerySet
 
 from source.models import Source
+from person.models import AgentDescription
+from event.models import Episode
+from letter.models import LetterDescription, GiftDescription
+from space.models import SpaceDescription
 
 def save_json(sources: QuerySet[Source], path: str | Path) -> None:
     data = _serialize(sources)
@@ -14,4 +18,53 @@ def save_json(sources: QuerySet[Source], path: str | Path) -> None:
 
 
 def _serialize(sources: QuerySet[Source]) -> Dict:
-    return {}
+    return {
+        'sources': [_serialize_source(source) for source in sources]
+    }
+
+def _serialize_source(source: Source) -> Dict:
+    agents = AgentDescription.objects.filter(source=source).values(
+        'id', 'name', 'description', 'is_group'
+    )
+    letters = LetterDescription.objects.filter(source=source).values(
+        'id', 'name', 'description'
+    )
+    gifts = GiftDescription.objects.filter(source=source).values(
+        'id', 'name', 'description',
+    )
+    locations = SpaceDescription.objects.filter(source=source).values(
+        'id', 'name', 'description'
+    )
+
+    return {
+        'id': source.pk,
+        'name': source.name,
+        'medieval_title': source.medieval_title,
+        'reference': source.reference,
+        'description': source.description_text,
+        'episodes': [
+            _serialize_episode(ep) for ep in
+            Episode.objects.filter(source=source).prefetch_related(
+                'categories', 'agents', 'gifts', 'letters', 'spaces'
+            )
+        ],
+        'agents': list(agents),
+        'letters': list(letters),
+        'gifts': list(gifts),
+        'locations': list(locations),
+    }
+
+
+def _serialize_episode(episode: Episode) -> Dict:
+    return {
+        'id': episode.pk,
+        'name': episode.name,
+        'summary': episode.summary,
+        'designators': episode.designators,
+        'labels': [label.name for label in episode.categories.all()],
+        'agents': [agent.pk for agent in episode.agents.all()],
+        'letters': [letter.pk for letter in episode.letters.all()],
+        'gifts': [gift.pk for gift in episode.gifts.all()],
+        'locations': [space.pk for space in episode.spaces.all()],
+    }
+
--- backend/download/tests/test_export_json.py
+++ backend/download/tests/test_export_json.py
@@ -3,11 +3,21 @@ import json
 from source.models import Source
 from download.export_json import save_json
 
-def test_export_json(tmp_path, source, episode):
+
+def test_export_json(tmp_path, source: Source, episode, episode_2):
     path = tmp_path / 'data.json'
-    save_json(Source.objects.all(), path)
+    qs = Source.objects.filter(pk=source.pk)
+    save_json(qs, path)
 
     with open(path) as f:
         data = json.load(f)
 
+    assert len(data['sources']) == 1
+    source_data = data['sources'][0]
+    assert source_data['name'] == source.name
+    assert len(source_data['episodes']) == 2
+
+    assert len(source_data['agents'])  == 2
+    assert len(source_data['letters']) == 1
+    assert source_data['letters'][0]['name'] == 'Bert\'s letter'
 
