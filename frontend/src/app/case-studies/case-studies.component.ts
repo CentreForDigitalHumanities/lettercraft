56f1f914f5ca701814d4ec554136643552edf295
Luka van der Plas <lukavdplas@mailfence.com>
2026-02-09 17:16:06 +0100

    show case studies list as aside

--- frontend/src/app/case-studies/case-studies.component.ts
+++ frontend/src/app/case-studies/case-studies.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ViewCaseStudiesGQL } from 'generated/graphql';
+import { map } from 'rxjs';
 
 @Component({
     selector: 'lc-case-studies',
@@ -7,5 +10,15 @@ import { Component } from '@angular/core';
     styleUrl: './case-studies.component.scss'
 })
 export class CaseStudiesComponent {
+    data$ = this.listQuery.watch().valueChanges.pipe(
+        map(result => result.data),
+    );
+
+    private routeID$ = this.route.params.pipe(map(params => params['id']));
+
+    constructor(
+        private route: ActivatedRoute,
+        private listQuery: ViewCaseStudiesGQL,
+    ) {}
 
 }
