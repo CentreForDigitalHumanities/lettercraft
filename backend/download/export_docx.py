951df04ad8b761d1bb550e8fd2af103d6d3d312d
Luka van der Plas <lukavdplas@mailfence.com>
2026-05-20 19:25:18 +0200

    add agents/letters etc to docx

--- backend/download/export_docx.py
+++ backend/download/export_docx.py
@@ -36,12 +36,22 @@ def _add_source(data: Dict, document: DocumentObject) -> None:
     if description := data["description"]:
         document.add_paragraph(description)
 
+    document.add_heading("Episodes", level=2)
     for episode in data["episodes"]:
         _add_episode(episode, data, document)
 
+    document.add_heading("Agents", level=2)
+    for agent in data["agents"]:
+        _add_agent(agent, document)
+
+    for key in ["letters", "gifts", "locations"]:
+        document.add_heading(key.capitalize(), level=2)
+        for agent in data[key]:
+            _add_entity(agent, document)
+
 
 def _add_episode(data: Dict, source_data: Dict, document: DocumentObject) -> None:
-    document.add_heading(data["name"], level=2)
+    document.add_heading(data["name"], level=3)
 
     if source_ref := _episode_source_ref(data["source_reference"]):
         document.add_paragraph(source_ref)
@@ -60,7 +70,11 @@ def _add_episode(data: Dict, source_data: Dict, document: DocumentObject) -> Non
         )
 
     for key in ["agents", "letters", "gifts", "locations"]:
-        _add_episode_entities(data, source_data, key, document)
+        if entities := _episode_entity_names(data, source_data, key):
+            document.add_paragraph()
+            document.add_paragraph(
+                f"{key}: {entities}".capitalize()
+            )
 
 
 def _episode_source_ref(data: Dict) -> str:
@@ -73,13 +87,6 @@ def _episode_source_ref(data: Dict) -> str:
     return ", ".join(parts).capitalize()
 
 
-def _add_episode_entities(episode_data: Dict, source_data: Dict, key: str, document: DocumentObject):
-    if entities := _episode_entity_names(episode_data, source_data, key):
-        document.add_paragraph(
-            f"{key}: {entities}".capitalize()
-        )
-
-
 def _episode_entity_names(episode_data: Dict, source_data: Dict, key: str):
     ids = episode_data[key]
     all_entities = source_data[key]
@@ -89,3 +96,22 @@ def _episode_entity_names(episode_data: Dict, source_data: Dict, key: str):
     ]
     names = [entity["name"] for entity in entities]
     return ", ".join(names)
+
+
+def _add_agent(data: Dict, document: DocumentObject):
+    p = document.add_paragraph(style="List Bullet")
+    name = p.add_run(data["name"])
+    if data["is_group"]:
+        name.add_text(" [group]")
+    name.add_text(". ")
+
+    description = p.add_run(data["description"])
+    description.italic = True
+
+
+def _add_entity(data: Dict, document: DocumentObject):
+    p = document.add_paragraph(style="List Bullet")
+    p.add_run(data["name"] + ". ")
+
+    description = p.add_run(data["description"])
+    description.italic = True
