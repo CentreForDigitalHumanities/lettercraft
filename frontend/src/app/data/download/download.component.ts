926579d8c141d2970db9c2291acaa4d5f129ea3f
Luka van der Plas <lukavdplas@mailfence.com>
2026-05-20 17:49:53 +0200

    modal in download component

--- frontend/src/app/data/download/download.component.ts
+++ frontend/src/app/data/download/download.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'lc-download',
@@ -7,5 +8,19 @@ import { Component } from '@angular/core';
     styleUrl: './download.component.scss'
 })
 export class DownloadComponent {
+    @ViewChild('downloadModal') modalTemplate?: TemplateRef<HTMLElement>;
 
+    modal: NgbModalRef | null = null;
+
+    constructor(
+        private modalService: NgbModal,
+    ) {}
+
+    open() {
+        this.modal = this.modalService.open(this.modalTemplate);
+    }
+
+    close() {
+        this.modal?.close();
+    }
 }
