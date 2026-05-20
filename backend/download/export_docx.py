db434bda3cd8164be58a5faa7f2539a64b83d426
Luka van der Plas <lukavdplas@mailfence.com>
2026-05-20 19:17:45 +0200

    add episode data

--- backend/download/export_docx.py
+++ backend/download/export_docx.py
@@ -28,30 +28,39 @@ def _add_source(data: Dict, document: DocumentObject) -> None:
     document.add_page_break()
     document.add_heading(data["name"])
 
-    if len(data["contributors"]):
+    if contributors := data["contributors"]:
         document.add_paragraph(
-            f"Contributors: {", ".join(data["contributors"])}"
+            f"Contributors: {", ".join(contributors)}"
         )
 
-    if data["description"]:
-        document.add_paragraph(data["description"])
+    if description := data["description"]:
+        document.add_paragraph(description)
 
     for episode in data["episodes"]:
-        _add_episode(episode, document)
+        _add_episode(episode, data, document)
 
 
-def _add_episode(data: Dict, document: DocumentObject) -> None:
+def _add_episode(data: Dict, source_data: Dict, document: DocumentObject) -> None:
     document.add_heading(data["name"], level=2)
 
-    source_ref = _episode_source_ref(data["source_reference"])
-    if source_ref:
+    if source_ref := _episode_source_ref(data["source_reference"]):
         document.add_paragraph(source_ref)
 
-    document.add_paragraph(data["summary"])
-    if len(data["designators"]):
+    if summary := data["summary"]:
+        document.add_paragraph(summary)
+
+    if labels := data["labels"]:
         document.add_paragraph(
-            f"Designators: {", ".join(data["designators"])}",
+            f"Labels: {", ".join(labels)}"
         )
+
+    if designators := data["designators"]:
+        document.add_paragraph(
+            f"Designators: {", ".join(designators)}",
+        )
+
+    for key in ["agents", "letters", "gifts", "locations"]:
+        _add_episode_entities(data, source_data, key, document)
 
 
 def _episode_source_ref(data: Dict) -> str:
@@ -62,3 +71,21 @@ def _episode_source_ref(data: Dict) -> str:
         if data[key] is not None
     ]
     return ", ".join(parts).capitalize()
+
+
+def _add_episode_entities(episode_data: Dict, source_data: Dict, key: str, document: DocumentObject):
+    if entities := _episode_entity_names(episode_data, source_data, key):
+        document.add_paragraph(
+            f"{key}: {entities}".capitalize()
+        )
+
+
+def _episode_entity_names(episode_data: Dict, source_data: Dict, key: str):
+    ids = episode_data[key]
+    all_entities = source_data[key]
+    entities = [
+        next(entity for entity in all_entities if entity["id"] == entity_id)
+        for entity_id in ids
+    ]
+    names = [entity["name"] for entity in entities]
+    return ", ".join(names)
