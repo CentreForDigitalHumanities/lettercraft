38b2c0d3d6d4986ce1802922c5f8fd4cfac95cab
Luka van der Plas <lukavdplas@mailfence.com>
2026-05-26 14:40:49 +0200

    single data source for export

--- backend/download/tests/test_export_docx.py
+++ backend/download/tests/test_export_docx.py
@@ -1,9 +1,11 @@
 from source.models import Source
+from download.export_json import json_data
 from download.export_docx import save_docx
 
 
 def test_export_json(tmp_path, source: Source, episode, episode_2, episode_attribution):
     path = tmp_path / "data.docx"
     qs = Source.objects.filter(pk=source.pk)
+    data = json_data(qs)
     with open(path, "wb") as f:
-        save_docx(qs, f)
+        save_docx(data, f)
